72d8ac0cf1c640dfe07b0cc7d7980e0da345cd1e
Arturo Espinosa <unammx@src.gnome.org>
1998-07-02 06:22:55 +0000

    nothing here, dont even bother cvs co this

---
+++ src/gnumeric-canvas.c
@@ -0,0 +1,7 @@
+#ifdef HAVE_CONFIG_H
+#   include <config.h>
+#endif
+
+#include <gnome.h>
+#include "gnumeric.h"
+#include "gnumeric-sheet.h"
---
+++ src/gnumeric.h
@@ -0,0 +1,4 @@
+#include <gmp.h>
+#include "style.h"
+#include "cell.h"
+#include "sheet.h"
