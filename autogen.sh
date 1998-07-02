b032505a04fbe9337076110d93f47c9cd2a7c86f
Arturo Espinosa <unammx@src.gnome.org>
1998-07-02 20:39:16 +0000

    setup sync

---
+++ autogen.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="Gnome Games"
+
+(test -f $srcdir/configure.in \
+  && test -d $srcdir/src \
+  && test -f $srcdir/src/gnumeric.h) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level gnumeric directory"
+    exit 1
+}
+
+. $srcdir/macros/autogen.sh
