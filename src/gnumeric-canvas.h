8ffc6306369c1f42f8c626ccae9ea09dbd2a7910
Arturo Espinosa <unammx@src.gnome.org>
1998-07-02 18:51:29 +0000

    Copy files trough CVS from laptop

---
+++ src/gnumeric-canvas.h
@@ -0,0 +1,27 @@
+#ifndef GNUMERIC_SHEET_H
+#define GNUMERIC_SHEET_H
+
+#define GNUMERIC_TYPE_SHEET     (gnumeric_sheet_get_type ())
+#define GNUMERIC_SHEET(obj)     (GTK_CHECK_CAST((obj), GNUMERIC_TYPE_SHEET, GnumericSheet))
+#define GNUMERIC_SHEET_CLASS(k) (GTK_CHECK_CLASS_CAST (k), GNUMERIC_TYPE_SHEET)
+#define GNUMERIC_IS_SHEET(o)    (GTK_CHECK_TYPE((o), GNUMERIC_TYPE_SHEET))
+
+typedef struct {
+	GnomeCanvas canvas;
+
+	Sheet       *sheet;
+	ColType     top_col;
+	RowType     top_row;
+
+	/* Font used for the labels in the columns and rows */
+	GdkFont     *label_font;
+} GnumericSheet;
+
+GtkType gnumeric_sheet_get_type (void);
+
+GnumericSheet *gnumeric_sheet_new (Sheet *sheet);
+
+typedef struct {
+	GnomeCanvasClass parent_class;
+} GnumericSheetClass;
+#endif
--- src/gnumeric-sheet.c
+++ src/gnumeric-sheet.c
@@ -5,3 +5,62 @@
 #include <gnome.h>
 #include "gnumeric.h"
 #include "gnumeric-sheet.h"
+
+/* Signals emited by the Gnumeric Sheet widget */
+enum {
+	GNUMERIC_SHEET_LAST_SIGNAL
+};
+
+static guint sheet_signals [GNUMERIC_SHEET_LAST_SIGNAL] = { 0 };
+
+static GnomeCanvasClass *sheet_parent_class;
+
+static void
+gnumeric_sheet_destroy (GtkObject *object)
+{
+	GnumericSheet *gsheet;
+
+	/* Add shutdown code here */
+
+	if (GTK_OBJECT_CLASS (sheet_parent_class)->destroy)
+		(*GTK_OBJECT_CLASS (sheet_parent_class)->destroy)(object);
+}
+
+GtkType
+gnumeric_sheet_get_type (void)
+{
+	static GtkType gnumeric_sheet_type = 0;
+
+	if (!gnumeric_sheet_type){
+		GtkTypeInfo gnumeric_sheet_info = {
+			"GnumericSheet",
+			sizeof (GnumericSheet),
+			sizeof (GnumericSheetClass),
+			(GtkClassInitFunc) gnumeric_sheet_class_init,
+			(GtkObjectInitFunc) NULL,
+			NULL, /* reserved 1 */
+			NULL, /* reserved 2 */
+			(GtkClassInitFunc) NULL
+		};
+
+		gnumeric_sheet_type = gtk_type_unique (gtk_object_get_type (), &gnumeric_sheet_info);
+	}
+
+	return gnumeric_sheet_type;
+}
+
+static void
+gnumeric_sheet_class_init (GnumericSheetClass *class)
+{
+	GtkObjectClass *object_class;
+	GtkWidgetClass *widget_class;
+	GnomeCanvasClass *canvas_class;
+
+	object_class = (GtkObjectClass *) class;
+	widget_class = (GtkWidgetClass *) class;
+
+	sheet_parent_class = gtk_type_class (gnome_canvas_get_type());
+
+	/* Method override */
+	object_class->destroy = gnumeric_sheet_destroy;
+}
