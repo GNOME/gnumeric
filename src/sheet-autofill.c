e08c86d02cced3bde3eab827719a8ef305254b82
Arturo Espinosa <unammx@src.gnome.org>
1998-09-14 05:04:53 +0000

    Updated TODO

--- src/sheet-autofill.c
+++ src/sheet-autofill.c
@@ -35,13 +35,14 @@ register_autofill_function (AutofillFunction fn)
 	autofill_functions = g_list_prepend (autofill_functions, fn);
 }
 
-static void
+static int
 autofill_integers (Sheet *sheet, int base_col, int base_row, int w, int h, int end_col, int end_row)
 {
+	return TRUE;
 }
 
 void
 autofill_init (void)
 {
-	register_autofill_function (autofill_integers);
+/*	register_autofill_function (autofill_integers); */
 }
