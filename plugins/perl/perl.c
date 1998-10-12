86089d2938adbaff88d355651d1b674ee8e72ff0
Tom Dyas <tdyas@src.gnome.org>
1998-10-12 21:21:28 +0000

    Sync sync. Changed how Python LIBS are detecting as per sample code posted by James Henstridge <james@daa.com.au>.
    Perl plugin should hopefully now build but the Makefile.am is horrible in
    terms of portability.
    
    The Gnumeric Perl extension needs to be installed manually. After normal
    build is done, just do "make install" in plugins/perl/ext.

--- plugins/perl/perl.c
+++ plugins/perl/perl.c
@@ -17,25 +17,26 @@ static PerlInterpreter *gnumeric_perl_interp;
 static int
 no_unloading_for_me (PluginData *pd)
 {
-    return 0;
+	return 0;
 }
 
 int
 init_plugin(PluginData *pd)
 {
-    char *argv[] = { "", NULL, NULL };
-    char *name;
+	char *argv[] = { "", NULL, NULL, NULL };
+	char *name;
 
-    /* Initialize Gnumeric plugin information. */
-    pd->can_unload = no_unloading_for_me;
-    pd->title = "Perl Plugin";
+	/* Initialize Gnumeric plugin information. */
+	pd->can_unload = no_unloading_for_me;
+	pd->title = "Perl Plugin";
 
-    /* Initialize the Perl interpreter. */
-    argv[1] = gnome_unconditional_datadir_file("gnumeric/perl/startup.pl");
-    gnumeric_perl_interp = perl_alloc();
-    perl_construct(gnumeric_perl_interp);
-    perl_parse(gnumeric_perl_interp, xs_init, 2, argv, NULL);
-    perl_run(gnumeric_perl_interp);
-    g_free(argv[1]);
+	/* Initialize the Perl interpreter. */
+	argv[1] = gnome_unconditional_datadir_file("gnumeric/perl/lib");
+	argv[2] = gnome_unconditional_datadir_file("gnumeric/perl/startup.pl");
+	gnumeric_perl_interp = perl_alloc();
+	perl_construct(gnumeric_perl_interp);
+	perl_parse(gnumeric_perl_interp, xs_init, 3, argv, NULL);
+	perl_run(gnumeric_perl_interp);
+
+	return 0;
 }
-
