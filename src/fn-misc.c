0b7537f8c08c115091c75b14c68b6fba7016520f
Miguel de Icaza <miguel@nuclecu.unam.mx>
1999-02-19 18:04:42 +0000

    Implement ISERROR.
    1999-02-19  Miguel de Icaza  <miguel@nuclecu.unam.mx>
    
    	* src/fn-misc.c (gnumeric_min): Implement ISERROR.

--- src/fn-misc.c
+++ src/fn-misc.c
@@ -24,6 +24,7 @@ static char *help_clean = {
 	   
 	   "@SEEALSO=")
 };
+
 static Value *
 gnumeric_clean (FunctionDefinition *fn, Value *argv [], char **error_string)
 {
@@ -52,7 +53,39 @@ gnumeric_clean (FunctionDefinition *fn, Value *argv [], char **error_string)
 	return res;
 }
 
+static char *help_iserror = {
+	N_("@FUNCTION=ISERROR\n"
+	   "@SYNTAX=ISERROR(exp)\n"
+
+	   "@DESCRIPTION="
+	   "Returns a TRUE value if the expression has an error\n"
+	   "\n"
+	   
+	   "@SEEALSO=")
+};
+
+static Value *
+gnumeric_iserror (void *tsheet, GList *expr_node_list, int eval_col, int eval_row, char **error_string)
+{
+	Value *v, *retval;
+	
+	if (g_list_length (expr_node_list) != 1){
+		*error_string = _("Argument mismatch");
+		return NULL;
+	}
+	v = eval_expr (tsheet, (ExprTree *) expr_node_list->data, eval_col, eval_row, error_string);
+	if (v == NULL)
+		retval = value_int (1);
+	else {
+		retval = value_int (0);
+		value_release (v);
+	}
+	
+	return retval;
+}
+
 FunctionDefinition misc_functions [] = {
-	{ "clean", "s",  "text",             &help_clean, NULL, gnumeric_clean },
+	{ "clean",   "s",  "text",             &help_clean, NULL, gnumeric_clean },
+	{ "iserror", "",   "",                 &help_iserror, gnumeric_iserror, NULL },
 	{ NULL, NULL }
 };
