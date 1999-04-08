666cefaef814c224ab8af291dacda7e3c549ca4e
Arturo Espinosa <unammx@src.gnome.org>
1999-04-08 18:45:48 +0000

    Add missing files

---
+++ src/solver.h
@@ -0,0 +1,42 @@
+#ifndef GNUMERIC_SOLVER_H
+#define GNUMERIC_SOLVER_H
+
+#include "sheet.h"
+
+
+typedef enum {
+        SolverMinimize, SolverMaximize, SolverEqualTo
+} SolverProblemType;
+
+
+typedef struct {
+        int                max_time_sec;
+        int                iterations;
+        float_t            precision;
+        float_t            tolerance;
+        float_t            convergence;
+        float_t            equal_to_value;
+        gboolean           assume_linear_model;
+        gboolean           assume_non_negative;
+        gboolean           automatic_scaling;
+        gboolean           show_iteration_results;
+} SolverOptions;
+
+typedef struct {
+        Cell               *lhs;       /* left hand side */
+        Cell               *rhs;       /* right hand side */
+        gchar              *type;      /* <=, =, >=, int, bool */
+        char               *str;       /* the same in string form */
+} SolverConstraint;
+
+typedef struct {
+        SolverProblemType  problem_type;
+        Cell               *target_cell;
+        CellList           *input_cells;
+        GSList             *constraints;
+        SolverOptions      options;
+} SolverParameters;
+
+
+#endif
+
