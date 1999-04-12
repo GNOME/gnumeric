a70fb8b6832b6a70ca319842d8e37a31d41b70e4
Michael Meeks <mmeeks@src.gnome.org>
1999-04-12 12:47:38 +0000

    Improved ole.c utility, and better organisation.

--- plugins/excel/ms-excel-biff.h
+++ plugins/excel/ms-excel-biff.h
@@ -56,52 +56,8 @@ typedef struct _BIFF_BOF_DATA
 extern BIFF_BOF_DATA *new_ms_biff_bof_data (BIFF_QUERY *pos) ;
 extern void free_ms_biff_bof_data (BIFF_BOF_DATA *data) ;
 
-/**
- * See S59D52.HTM for the spec.
- * As you add low numbers, check you don't glup an odd-ball
- **/
+#include "biff-types.h"
 
-#define BIFF_DIMENSIONS                 0x00
-#define BIFF_BLANK                      0x01
-#define BIFF_NUMBER                     0x03
-#define BIFF_LABEL                      0x04
-#define BIFF_FORMULA                    0x06
-#define BIFF_ROW                        0x08
-#define BIFF_BOF                        0x09
-#define BIFF_EOF                        0x0a
-#define BIFF_PRECISION                  0x0e
-#define BIFF_HEADER                     0x14
-#define BIFF_FOOTER                     0x15
-#define BIFF_EXTERNCOUNT                0x16 /* number of external references*/
-#define BIFF_EXTERNSHEET                0x17
-#define BIFF_SELECTION                  0x1d
-#define BIFF_FORMAT                     0x1e
-#define BIFF_ARRAY                      0x21
-#define BIFF_EXTERNNAME                 0x23
-#define BIFF_FONT                       0x31
-#define BIFF_XF_OLD                     0x43
-
-#define BIFF_COLINFO                    0x7d
-#define BIFF_RK                         0x7e
-#define BIFF_BOUNDSHEET                 0x85
-#define BIFF_PALETTE                    0x92
-#define BIFF_SHRFMLA                    0xbc
-#define BIFF_MULRK                      0xbd
-#define BIFF_MULBLANK                   0xbe
-#define BIFF_RSTRING                    0xd6
-#define BIFF_DBCELL                     0xd7
-#define BIFF_XF                         0xe0
-
-#define BIFF_SST                        0xfc
-#define BIFF_LABELSST                   0xfd
-
-/* Odd balls */
-#define BIFF_SUPBOOK                   0x1ae /* Supporting Workbook */
-#define BIFF_DV                        0x1be
-#define BIFF_BOOLERR                   0x205
-#define BIFF_STRING                    0x207
-#define BIFF_NAME                      0x218
-#define BIFF_WINDOW2                   0x23e
 #endif
 
 
--- plugins/excel/ole.c
+++ plugins/excel/ole.c
@@ -1,8 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "ms-ole.h"
+#include "ms-biff.h"
+
+#define TYPES_FILE "biff-types.h"
+
+typedef struct {
+	guint16 opcode;
+	char *name;
+} BIFF_TYPE;
+
+static GPtrArray *types=NULL;
+
+static void
+read_types ()
+{
+	FILE *file = fopen(TYPES_FILE, "r");
+	char buffer[1024];
+	types = g_ptr_array_new ();
+	while (!feof(file)) {
+		char *p;
+		fgets(buffer,1023,file);
+		for (p=buffer;*p;p++)
+			if (*p=='0' && *(p+1)=='x') {
+				BIFF_TYPE *bt = g_new (BIFF_TYPE,1);
+				char *name, *pt;
+				bt->opcode=strtol(p+2,0,16);
+				pt = buffer;
+				while (*pt && *pt != '#') pt++;      /* # */
+				while (*pt && !isspace(*pt)) pt++;  /* define */
+				while (*pt &&  isspace(*pt)) pt++;  /* '   ' */
+				name = pt;
+				while (*pt && !isspace(*pt)) pt++;
+				bt->name=g_strndup(name, (pt-name));
+				g_ptr_array_add (types, bt);
+				break;
+			}
+	}
+	fclose (file);
+}
+
+static char*
+get_opcode_name (guint16 opcode)
+{
+	int lp;
+	if (!types)
+		read_types ();
+	for (lp=0;lp<types->len;lp++) {
+		BIFF_TYPE *bt = g_ptr_array_index (types, lp);
+		if (bt->opcode>0xff) {
+			if (bt->opcode == opcode)
+				return bt->name;
+		} else {
+			if (bt->opcode == (opcode&0xff))
+				return bt->name;
+		}
+	}
+	return "Unknown";
+}
 
 static MS_OLE_DIRECTORY *
 get_file_handle (MS_OLE *ole, char *name)
@@ -62,6 +120,9 @@ int main (int argc, char **argv)
 	if (!ole)
 		syntax_error ("Can't open file");
 
+	if (argc<=2)
+		syntax_error ("Need command or -i");
+
 	if (argc>2 && argv[argc-1][0]=='-'
 	    && argv[argc-1][1]=='i') 
 		interact=1;
@@ -100,6 +161,22 @@ int main (int argc, char **argv)
 					dump (buffer, dir->length);
 				else
 					printf ("Failed read\n");
+				ms_ole_stream_close (stream);
+			} else {
+				printf ("Need a stream name\n");
+				return 0;
+			}
+		} else if (g_strcasecmp(ptr, "biff")==0) {
+			MS_OLE_DIRECTORY *dir;
+			ptr = strtok (NULL, delim);
+			if ((dir = get_file_handle (ole, ptr)))
+			{
+				MS_OLE_STREAM *stream = ms_ole_stream_open (dir, 'r');
+				BIFF_QUERY *q = ms_biff_query_new (stream);
+				while (ms_biff_query_next(q)) {
+					printf ("Opcode 0x%3x : %15s, length %d\n",
+						q->opcode, get_opcode_name (q->opcode), q->length);
+				}
 				ms_ole_stream_close (stream);
 			} else {
 				printf ("Need a stream name\n");
