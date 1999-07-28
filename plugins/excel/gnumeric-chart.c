e582115b1867dabdb942189963b14028000f35ad
Jody Goldberg <jody@src.gnome.org>
1999-07-28 01:19:56 +0000

    Add new file.

---
+++ plugins/excel/gnumeric-chart.c
@@ -0,0 +1,15 @@
+/**
+ * gnumeric-chart.c: Gnumeric chart object
+ *
+ * I'll lurk it down here until it firms up a bit.
+ *
+ * Author:
+ *    Jody Goldberg (jgoldberg@home.com)
+ **/
+
+#include "gnumeric-chart.h"
+
+void
+gnumeric_chart_destroy (GnumericChart * chart)
+{
+}
