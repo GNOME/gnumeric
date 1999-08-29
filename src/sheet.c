afdba23709f497dedea9d9aa89cd82b47d39c5b2
Arturo Espinosa <unammx@src.gnome.org>
1999-08-29 07:34:28 +0000

    Fixed small problem

--- src/sheet.c
+++ src/sheet.c
@@ -3864,10 +3864,6 @@ sheet_cursor_set (Sheet *sheet, int base_col, int base_row, int start_col, int s
 	}
 	sheet_load_cell_val (sheet);
 
-	/*
-	 * For speach synthesis
-	 */
-	workbook_cursor_moved (sheet->workbook, base_col, base_row, start_col, start_row, end_col, end_row);
 }
 
 /**
