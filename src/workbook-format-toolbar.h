52e448bf7102a236c716ced5f2662f86d55d6b60
Arturo Espinosa <unammx@src.gnome.org>
1999-09-22 05:16:17 +0000

    Added missing file

---
+++ src/workbook-format-toolbar.h
@@ -0,0 +1,9 @@
+#ifndef GNUMERIC_WORKBOOK_FORMAT_TOOLBAR_H
+#define GNUMERIC_WORKBOOK_FORMAT_TOOLBAR_H
+
+#define TOOLBAR_BOLD_BUTTON_INDEX 2
+#define TOOLBAR_ITALIC_BUTTON_INDEX 3
+
+GtkWidget *workbook_create_format_toolbar (Workbook *wb);
+
+#endif
