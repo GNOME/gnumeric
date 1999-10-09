6c1018630df7792adb9d02543adfbbdc2ade4430
Jody Goldberg <jody@src.gnome.org>
1999-10-09 16:00:49 +0000

    Port my border patch to the new style code. Import is failing, and I have not enabled the draw code yet.

---
+++ src/style-border.h
@@ -0,0 +1,44 @@
+#ifndef GNUMERIC_BORDER_H
+#define GNUMERIC_BORDER_H
+
+#include "mstyle.h"
+
+typedef enum {
+ 	BORDER_NONE			= 0x0,
+ 	BORDER_THIN			= 0x1,
+ 	BORDER_MEDIUM			= 0x2,
+ 	BORDER_DASHED			= 0x3,
+ 	BORDER_DOTTED			= 0x4,
+ 	BORDER_THICK			= 0x5,
+ 	BORDER_DOUBLE			= 0x6,
+ 	BORDER_HAIR			= 0x7,
+	BORDER_MEDIUM_DASH		= 0x8,
+	BORDER_DASH_DOT			= 0x9,
+	BORDER_MEDIUM_DASH_DOT		= 0xa,
+	BORDER_DASH_DOT_DOT		= 0xb,
+	BORDER_MEDIUM_DASH_DOT_DOT	= 0xc,
+	BORDER_SLANTED_DASH_DOT		= 0xd,
+
+ 	BORDER_MAX
+} StyleBorderType;
+
+struct _MStyleBorder {
+	/* Key elements */
+	StyleBorderType	 line_type;
+	StyleColor     	*color;
+	gboolean	 is_vertical;
+
+	/* Private */
+	GdkGC	*gc;
+	gint	ref_count;
+};
+
+void		border_ref (MStyleBorder *border);
+void		border_unref (MStyleBorder *border);
+MStyleBorder *	border_fetch (StyleBorderType const	 line_type,
+			      StyleColor 		*color,
+			      MStyleElementType const	 orientation);
+
+GdkGC *	border_get_gc (MStyleBorder * border, GdkWindow * window);
+
+#endif /* GNUMERIC_BORDER_H */
