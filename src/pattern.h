7dafc4188df7de2e7002ad41a2f3075231896178
Jody Goldberg <jody@src.gnome.org>
1999-11-04 00:45:45 +0000

    Split the pattern code into a seperate file

---
+++ src/pattern.h
@@ -0,0 +1,10 @@
+#ifndef GNUMERIC_PATTERN_H
+#define GNUMERIC_PATTERN_H
+
+#include "mstyle.h"
+
+#define GNUMERIC_SHEET_PATTERNS 18
+
+GdkPixmap * gnumeric_pattern_get_stipple (gint const index);
+
+#endif /* GNUMERIC_PATTERN_H */
