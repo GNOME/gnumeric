2bb837382d0bf7862ccf9b52a7dec7398196801e
Jody Goldberg <jody@src.gnome.org>
1999-11-15 00:37:42 +0000

    Rewrite CSV code.

--- plugins/ff-csv/csv-io.c
+++ plugins/ff-csv/csv-io.c
@@ -1,67 +1,145 @@
 /*
- * csv-io.c: save/read Sheets using a CSV encoding.
+ * csv-io.c: read sheets using a CSV encoding.
  *
  * Miguel de Icaza <miguel@gnu.org>
- *
+ * Jody Goldberg   <jgoldberg@home.com>
  */
 
 #include <config.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <errno.h>
 #include <gnome.h>
 #include "plugin.h"
 #include "gnumeric.h"
 #include "file.h"
-#include "libcsv.h"
 
-
-static void
-load_table_into_sheet (struct csv_table *table, Sheet *sheet)
+struct FileSource
 {
-	int row;
+	char const *data, *cur;
+	int         len;
 
-	for (row = 0; row < table->height; row++){
-		Cell *cell;
-		int col;
+	int line;
+	Sheet *sheet;
+};
 
-		for (col = 0; col < CSV_WIDTH (table, row); col++){
-			cell = sheet_cell_new (sheet, col, row);
-			cell_set_text_simple (cell, CSV_ITEM (table, row, col));
+static char *
+csv_parse_field (struct FileSource *src)
+{
+	GString *res = NULL;
+	char const *cur = src->cur;
+	char const delim =
+		(*cur != '"' && *cur != '\'')
+		? ',' : *cur;
+	char const * const start = (delim == ',') ? cur : ++cur;
+
+	while (*cur && *cur != delim && *cur != '\n' && *cur != '\r') {
+		if (*cur == '\\') {
+			/* If this is the first escape character get setup */
+			if (start != cur) {
+				char *tmp = g_strndup (start, cur-start);
+				res = g_string_new (tmp);
+				g_free (tmp);
+			} else
+				res = g_string_new ("");
+
+			if (!cur[1]) {
+				g_warning ("CSV : Unexpected end of line at line %d", src->line);
+				return NULL;
+			}
+
+			g_string_append_c (res, cur[1]);
+			cur += 2;
+		} else {
+			if (res != NULL)
+				g_string_append_c (res, *cur);
+			++cur;
 		}
-			
 	}
+
+	/* Skip close delemiter */
+	if (*cur) {
+		src->cur = cur;
+		if (delim != ',')
+			++src->cur;
+		if (*src->cur == ',')
+			++src->cur;
+	}
+
+	if (res != NULL) {
+		char *tmp = res->str;
+		g_string_free (res, FALSE);
+		return tmp;
+	}
+
+	return g_strndup (start, cur-start);
 }
 
 static gboolean
-csv_read_workbook (Workbook *book, const char* filename)
+csv_parse_sheet (struct FileSource *src)
 {
-	struct csv_table table;
-	Sheet *sheet;
-	FILE *f;
-	char *name;
+	int row, col;
+	char *field;
 
-	f = fopen (filename, "r");
-	if (f == NULL)
-		return FALSE;
-	
-	if (csv_load_table (f, &table) == -1)
-		return FALSE;
-
-	fclose (f);
-	
-
-	name = g_strdup_printf (_("Imported %s"), g_basename (filename));
-	sheet = sheet_new (book, name);
-	g_free (name);
-	
-	workbook_attach_sheet (book, sheet);
-
-	load_table_into_sheet (&table, sheet);
-	
-	csv_destroy_table (&table);
-	
+	for (row = 0 ; *src->cur ; ++row, ++src->line, ++(src->cur)) {
+		for (col = 0 ; *src->cur && *src->cur != '\n' && *src->cur != '\r' ; ++col)
+			if (NULL != (field = csv_parse_field (src))) {
+				Cell *cell = sheet_cell_new (src->sheet, col, row);
+				cell_set_text_simple (cell, field);
+				g_free (field);
+			} else
+				return FALSE;
+	}
 	return TRUE;
 }
 
+static gboolean
+csv_read_workbook (Workbook *book, char const *filename)
+{
+	/* TODO : When there is a reasonable error reporting
+	 * mechanism use it and put all the error code back
+	 */
+	gboolean result = FALSE;
+	int len;
+	struct stat sbuf;
+	char const *data;
+	int const fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return FALSE;
+
+	if (fstat(fd, &sbuf) < 0) {
+		close (fd);
+		return FALSE;
+	}
+
+	len = sbuf.st_size;
+	if (MAP_FAILED != (data = (char const *) (mmap(0, len, PROT_READ,
+						       MAP_PRIVATE, fd, 0)))) {
+		struct FileSource src;
+		char * name = g_strdup_printf (_("Imported %s"), g_basename (filename));
+
+		src.data  = data;
+		src.cur   = data;
+		src.len   = len;
+		src.sheet = sheet_new (book, name);
+
+		workbook_attach_sheet (book, src.sheet);
+		g_free (name);
+
+		result = csv_parse_sheet (&src);
+
+		munmap((char *)data, len);
+	}
+	close(fd);
+
+	return result;
+}
 
 static int
 csv_can_unload (PluginData *pd)
@@ -83,6 +161,6 @@ init_plugin (PluginData * pd)
 	pd->can_unload = csv_can_unload;
 	pd->cleanup_plugin = csv_cleanup_plugin;
 	pd->title = g_strdup (_("Comma Separated Value (CSV) module"));
-	
+
 	return 0;
 }
