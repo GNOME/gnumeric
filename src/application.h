eceb72c28efdbcb2e495b120558b6fa8d64fb456
Jody Goldberg <jody@src.gnome.org>
2000-01-02 23:03:19 +0000

    Fix warning

--- src/application.h
+++ src/application.h
@@ -10,6 +10,7 @@ void         application_clipboard_clear	(void);
 void         application_clipboard_copy		(Sheet *sheet, Range const *area);
 void         application_clipboard_cut		(Sheet *sheet, Range const *area);
 
+void	     application_clipboard_unant        (void);
 gboolean     application_clipboard_is_empty	(void);
 Sheet *      application_clipboard_sheet_get	(void);
 CellRegion * application_clipboard_contents_get	(void);
