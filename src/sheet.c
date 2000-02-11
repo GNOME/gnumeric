0dfb3a786c1ee309c44a466824fa9db6496c24a3
Michael Meeks <mmeeks@src.gnome.org>
2000-02-11 22:19:19 +0000

    Brain deadness in signal parameter order.

--- src/sheet.c
+++ src/sheet.c
@@ -1425,8 +1425,8 @@ sheet_load_cell_val (Sheet *sheet)
 		text = g_strdup ("");
 
 	gtk_entry_set_text (entry, text);
-	gtk_signal_emit_by_name (sheet->workbook, "cell_changed", sheet,
-				 sheet->cursor_col, sheet->cursor_row, text);
+	gtk_signal_emit_by_name (sheet->workbook, "cell_changed", sheet, text,
+				 sheet->cursor_col, sheet->cursor_row);
 
 	g_free (text);
 }
