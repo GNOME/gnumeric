8bf014a88973f6a1a4ae29759eb8b89445205581
JP Rosevear <jpr@arcavia.com>
2000-06-30 20:47:09 +0000

    Use consistent names.
    2000-06-30  JP Rosevear  <jpr@arcavia.com>
    
    	* src/embeddable-grid.c (EmbeddableGridFactory_init): Use
    consistent names.
    
    	* gnumeric.gnorba: Make server names consistent
    
    	* gnumeric.oafinfo: Make server names consistent

--- src/embeddable-grid.c
+++ src/embeddable-grid.c
@@ -283,11 +283,11 @@ EmbeddableGridFactory_init (void)
 {
 #if USING_OAF
 	bonobo_embeddable_grid_factory = bonobo_embeddable_factory_new (
-		"OAFIID:GOADID:Gnumeric:GridFactory:1.0:a1c2ad1c-33bd-4c42-b5d1-9a454f862873",
+		"OAFIID:GOADID:GNOME:Gnumeric:GridFactory:1.0:a1c2ad1c-33bd-4c42-b5d1-9a454f862873",
 		embeddable_grid_factory, NULL);
 #else
 	bonobo_embeddable_grid_factory = bonobo_embeddable_factory_new (
-		"GNOME:Gnumeric:GridFactory:1.0",
+		"GOADID:GNOME:Gnumeric:GridFactory:1.0",
 		embeddable_grid_factory, NULL);
 #endif
 }
