3b0a67ecb20c606ab2b1feb0302c68aa054b173a
Jody Goldberg <jody@src.gnome.org>
2000-09-21 14:13:15 +0000

    warning suppression.

--- src/position.c
+++ src/position.c
@@ -46,7 +46,7 @@ eval_pos_init_dep (EvalPos *eval_pos, Dependent const *dep)
 	g_return_val_if_fail (dep != NULL, NULL);
 
 	if (DEPENDENT_CELL == (dep->flags & DEPENDENT_TYPE_MASK)) {
-		Cell const *cell = CELL_TO_DEP (dep);
+		Cell const *cell = DEP_TO_CELL (dep);
 		return eval_pos_init (eval_pos, dep->sheet, &cell->pos);
 	} else {
 		static CellPos const pos = { 0, 0 };
