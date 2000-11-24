e57a0c66eee7de628215bf32311341c42e7687e9
Ariel Rios <ariel@arcavia.com>
2000-11-24 02:30:05 +0000

    Removed unused prototypes. (gnumeric_scm2list): Ditto (expr_to_scm): Ditto
    2000-11-23  Ariel Rios  <ariel@arcavia.com>
    
    * guile-support.h (gnumeric_list2scm): Removed unused prototypes.
    (gnumeric_scm2list): Ditto
     (expr_to_scm): Ditto
    * guile-support.c (gnumeric_list2scm): Removed unused function.
    (gnumeric_scm2list): Ditto

--- plugins/guile/guile-support.h
+++ plugins/guile/guile-support.h
@@ -6,9 +6,7 @@
 #include "gnumeric.h"
 #include "value.h"
 
-SCM gnumeric_list2scm (GList *list, SCM (*data_to_scm)(void*));
-GList *gnumeric_scm2list (SCM obj , void (*fromscm)(SCM, void*));
 SCM value_to_scm (Value const *val, CellRef cell_ref);
 Value* scm_to_value (SCM scm);
-SCM expr_to_scm (ExprTree *expr, CellRef cell_ref);
+
 
