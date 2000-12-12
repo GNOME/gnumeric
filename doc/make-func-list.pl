e8848467d90febe8095228b1f6efb8b584ada486
Morten Welinder <mortenw@src.gnome.org>
2000-12-12 00:04:41 +0000

    Quote < and >.
    (Blind change.  I hope nothing breaks, :-)

--- doc/make-func-list.pl
+++ doc/make-func-list.pl
@@ -1,77 +1,85 @@
 $state = 0;
 
 while (<>){
-  if (/^\@FUNCTION=(.*)/){
+    s/\s+$//;
+    if (/^\@FUNCTION=(.*)/){
+	if ($state){
+	    printf "\n";
+	    print "      </refsect1>\n";
+	    print "    </refentry>\n\n";
+	}
+	my $func = $1;
+	$state = 0;
+	print "\n\n";
+	print "  <refentry>\n";
+	print "    <refmeta>\n";
+	print "      <refentrytitle><anchor id=\"gnumeric-$func\">$func</refentrytitle><refmiscinfo></refmiscinfo>\n";
+	print "    </refmeta>\n";
+	print "    <refnamediv>\n";
+	print "      <refname>$func</refname>\n";
+	print "      <refpurpose></refpurpose>\n";
+	print "    </refnamediv>\n";
+	next;
+    }	
+    
+    if (/^\@SYNTAX=(.*)/){
+	print "    <refsynopsisdiv>\n";
+	print "      <synopsis>", &quote_stuff ($1), "</synopsis>\n";
+	print "    </refsynopsisdiv>\n";
+	next;
+    }
+    
+    if (/^\@DESCRIPTION=(.*)/){
+	print "      <refsect1>\n";
+	print "        <title>Description</title>\n";
+	print "        <para>", &quote_stuff ($1), "</para>\n";
+	$state = 1;
+	next;
+    } 
+
+    if (/^\@EXAMPLES=(.*)/){
+	if ($state){
+	    print "\n    </refsect1>";
+	}
+	print "      <refsect1>\n";
+	print "        <title>Examples</title>\n";
+	print "        <para>", &quote_stuff ($1), "</para>\n";
+	$state = 2;
+	next;
+    } 
+    if (/^\@SEEALSO=(.*)/){
+	my @links = split (/,/, $1);
+
+	if ($state){
+	    print "\n    </refsect1>";
+	}
+	print "\n    <refsect1><title>See also</title>\n";
+	@a = ();
+	print   "      <para>";
+	foreach my $link (@links){
+	    $link =~ s/\s//g;
+
+	    push @a, "        <link linkend=\"gnumeric-$link\">$link</link>\n";
+	}
+	print join (", ", @a);
+	print "      </para>\n";
+	print "    </refsect1>\n";
+	print "  </refentry>\n\n";
+	$state = 0;
+	next;
+    }
+
     if ($state){
-	printf "\n";
-        print "      </refsect1>\n";
-        print "    </refentry>\n\n";
+	print "        <para>", &quote_stuff ($_), "</para>";
+    } else {
     }
-    $func=$1;
-    $state = 0;
-    print "\n\n";
-    print "  <refentry>\n";
-    print "    <refmeta>\n";
-    print "      <refentrytitle><anchor id=\"gnumeric-$1\">$1</refentrytitle><refmiscinfo></refmiscinfo>\n";
-    print "    </refmeta>\n";
-    print "    <refnamediv>\n";
-    print "      <refname>$1</refname>\n";
-    print "      <refpurpose></refpurpose>\n";
-    print "    </refnamediv>\n";
-    next;
-  }	
-  
-  if (/^\@SYNTAX=(.*)/){
-    print "    <refsynopsisdiv>\n";
-    print "      <synopsis>$1</synopsis>\n";
-    print "    </refsynopsisdiv>\n";
-    next;
-  }
-  
-  if (/^\@DESCRIPTION=(.*)/){
-    print "      <refsect1>\n";
-    print "        <title>Description</title>\n";
-    print "        <para>$1</para>\n";
-    $state = 1;
-    next;
-  } 
+}
 
-  if (/^\@EXAMPLES=(.*)/){
-    if ($state){
-      print "\n    </refsect1>";
-    }
-    print "      <refsect1>\n";
-    print "        <title>Examples</title>\n";
-    print "        <para>$1</para>\n";
-    $state = 2;
-    next;
-  } 
-  if (/^\@SEEALSO=(.*)/){
-    @links = split (/,/, $1);
 
-    if ($state){
-      print "\n    </refsect1>";
-    }
-    print "\n    <refsect1><title>See also</title>\n";
-    @a = ();
-    print   "      <para>";
-    foreach $link  (@links){
-      $link =~ s/ //g;
+sub quote_stuff {
+    my ($str) = @_;
 
-       push @a, "        <link linkend=\"gnumeric-$link\">$link</link>\n";
-
-    }
-    print join (", ", @a);
-    print "      </para>\n";
-    print "    </refsect1>\n";
-    print "  </refentry>\n\n";
-    $state = 0;
-    next;
-  }
-
-  if ($state){
-    chop;
-    print "        <para>$_</para>";
-  } else {
-  }
+    $str =~ s/</\&lt;//g;
+    $str =~ s/>/\&gt;//g;
+    return $str;
 }
