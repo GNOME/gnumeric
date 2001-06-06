850b693d4c51e3fab5a54745f0795ee633f43b8a
Almer S. Tigelaar <almer@gnome.org>
2001-06-06 19:12:26 +0000

    Include config.h
    2001-06-06  Almer S. Tigelaar <almer@gnome.org>
    
    	* src/consolidate.c : Include config.h

--- src/consolidate.c
+++ src/consolidate.c
@@ -17,7 +17,8 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-
+ 
+#include <config.h>
 #include "cell.h"
 #include "eval.h"
 #include "func.h"
