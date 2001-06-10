85a10635c2c89a7f92616c0ae13ecb9e7e56b8a0
Morten Welinder <mortenw@src.gnome.org>
2001-06-10 15:14:53 +0000

    Precision 0.005.  Fix some test texts.

--- src/functions/fn-database.c
+++ src/functions/fn-database.c
@@ -1010,7 +1010,7 @@ gnumeric_dstdev (FunctionEvalInfo *ei, Value **argv)
 	if (p.N - 1 == 0)
 		return value_new_error (ei->pos, gnumeric_err_NUM);
 
-        return value_new_float (sqrt(p.Q / (p.N - 1)));
+        return value_new_float (sqrt (p.Q / (p.N - 1)));
 }
 
 /***************************************************************************/
@@ -1078,7 +1078,7 @@ gnumeric_dstdevp (FunctionEvalInfo *ei, Value **argv)
 	if (p.N == 0)
 		return value_new_error (ei->pos, gnumeric_err_NUM);
 
-        return value_new_float (sqrt(p.Q / p.N));
+        return value_new_float (sqrt (p.Q / p.N));
 }
 
 /***************************************************************************/
