1f87079576f2e6be3e38b9aaf3ec0b6942ae64d2
Almer S. Tigelaar <almer@gnome.org>
2001-10-22 16:59:50 +0000

    Re-Order validation Enum and define new StyleCondition types.
    2001-10-22  Almer S. Tigelaar  <almer@gnome.org>
    
    	* src/style-condition.h :
    	Re-Order validation Enum and define new StyleCondition
    	types.
    
    	* src/style-condition.c (style_condition_expr_dep_eval) :
    	Renamed from style_condition_dep_eval.
    	(style_condition_expr_set_expr) :
    	Renamed from style_condition_set_expr.
    	(style_condition_expr_dep_debug_name) :
    	Renamed from style_condition_dep_debug_name.
    	(style_condition_new_expr): Renamed from
    	style_condition_new.
    	(style_condition_new_constraint): New, creates a
    	stylecondition of type constraint.
    	(style_condition_new_flags): New, creates a stylecondition
    	of type flags.
    	(style_condition_unref): Deal with changes in stylecondition
    	structure.
    	(style_condition_chain): Add operator parameter.
    	(style_condition_expr_eval): Split out from style_condition_eval.
    	(style_condition_constraint_eval): Idem.
    	(style_condition_flags_eval): New, handles allow blank.
    	(style_condition_dump): New, dumps a style condition chain to the
    	screen. For debugging purposes.
    	(style_condition_eval): Handle the new chain operators and use
    	the *_eval functions.

--- src/style-condition.c
+++ src/style-condition.c
@@ -27,19 +27,19 @@
 
 /*********************************************************************************/
 
-#define DEP_TO_STYLE_CONDITION(d_ptr) (StyleCondition *)(((char *)d_ptr) - GTK_STRUCT_OFFSET(StyleCondition, dep))
+#define DEP_TO_STYLE_CONDITION_EXPR(d_ptr) (StyleConditionExpr *)(((char *)d_ptr) - GTK_STRUCT_OFFSET(StyleConditionExpr, dep))
 
 static void
-style_condition_dep_eval (Dependent *dep)
+style_condition_expr_dep_eval (Dependent *dep)
 {
-	StyleCondition *sc;
+	StyleConditionExpr *sce;
 	
 	g_return_if_fail (dep != NULL);
-	sc = DEP_TO_STYLE_CONDITION (dep);
+	sce = DEP_TO_STYLE_CONDITION_EXPR (dep);
 	
-	if (sc->val) {
-		value_release (sc->val);
-		sc->val = NULL;
+	if (sce->val) {
+		value_release (sce->val);
+		sce->val = NULL;
 	}
 		
 	if (dep->expression) {
@@ -48,58 +48,96 @@ style_condition_dep_eval (Dependent *dep)
 		ep.eval.row = ep.eval.col = 0;
 		ep.sheet = dep->sheet;
 			
-		sc->val = eval_expr (&ep, dep->expression, 0);
+		sce->val = eval_expr (&ep, dep->expression, 0);
 	}
 }
 
 static void
-style_condition_dep_set_expr (Dependent *dep, ExprTree *new_expr)
+style_condition_expr_dep_set_expr (Dependent *dep, ExprTree *new_expr)
 {
-	StyleCondition *sc = DEP_TO_STYLE_CONDITION (dep);
+	StyleConditionExpr *sce = DEP_TO_STYLE_CONDITION_EXPR (dep);
 	
 	/* Make sure no invalid 'cached' value
 	 * of the previous expression remains
 	 */
-	if (sc->val) {
-		value_release (sc->val);
-		sc->val = NULL;
+	if (sce->val) {
+		value_release (sce->val);
+		sce->val = NULL;
 	}
 }
 
 static void
-style_condition_dep_debug_name (Dependent const *dep, FILE *out)
+style_condition_expr_dep_debug_name (Dependent const *dep, FILE *out)
 {
 	g_return_if_fail (dep != NULL);
 	
 	fprintf (out, "StyleCondition Dep %p", dep);
 }
 
-static DEPENDENT_MAKE_TYPE (style_condition_dep, &style_condition_dep_set_expr)
+static DEPENDENT_MAKE_TYPE (style_condition_expr_dep, &style_condition_expr_dep_set_expr)
 
 /*********************************************************************************/
 
 StyleCondition *
-style_condition_new (Sheet *sheet, StyleConditionOp op, ExprTree *expr)
+style_condition_new_expr (Sheet *sheet, StyleConditionOperator op,
+			  ExprTree *expr)
 {
 	StyleCondition *sc;
 
-	/* Be paranoid, expr is only needed for value restrictions */
-	if (op >= STYLE_CONDITION_EQUAL && op <= STYLE_CONDITION_LESS_EQUAL)
-		g_return_val_if_fail (expr != NULL, NULL);
+	g_return_val_if_fail (expr != NULL, NULL);
 	g_return_val_if_fail (IS_SHEET (sheet), NULL);
 	
 	sc = g_new0 (StyleCondition, 1);
 
-	sc->ref_count = 1;	
-	sc->op        = op;
-	sc->dep.sheet = sheet;
-	sc->dep.flags = style_condition_dep_get_dep_type ();
-	dependent_set_expr (&sc->dep, expr);
-
-	sc->val  = NULL;
-	sc->next = NULL;
+	sc->type      = SCT_EXPR;
+	sc->ref_count = 1;
+	
+	sc->u.expr.op        = op;
+	sc->u.expr.dep.sheet = sheet;
+	sc->u.expr.dep.flags = style_condition_expr_dep_get_dep_type ();
+	dependent_set_expr (&sc->u.expr.dep, expr);
+	sc->u.expr.val = NULL;
+	
+	sc->next    = NULL;
+	sc->next_op = SCB_NONE;
 		
-	return sc; 
+	return sc;
+}
+
+StyleCondition *
+style_condition_new_constraint (StyleConditionConstraint constraint)
+{
+	StyleCondition *sc;
+
+	sc = g_new0 (StyleCondition, 1);
+
+	sc->type      = SCT_CONSTRAINT;
+	sc->ref_count = 1;
+
+	sc->u.constraint = SCC_IS_INT;
+	
+	sc->next    = NULL;
+	sc->next_op = SCB_NONE;
+
+	return sc;
+}
+
+StyleCondition *
+style_condition_new_flags (StyleConditionFlags flags)
+{
+	StyleCondition *sc;
+
+	sc = g_new0 (StyleCondition, 1);
+
+	sc->type      = SCT_FLAGS;
+	sc->ref_count = 1;
+
+	sc->u.flags = flags;
+	
+	sc->next    = NULL;
+	sc->next_op = SCB_NONE;
+
+	return sc;
 }
 
 void
@@ -123,110 +161,289 @@ style_condition_unref (StyleCondition *sc)
 		if (sc->next)
 			style_condition_unref (sc->next);
 
-		dependent_set_expr (&sc->dep, NULL);
-		if (sc->val) {
-			value_release (sc->val);
-			sc->val = NULL;
-		}
+		if (sc->type == SCT_EXPR) {
+			dependent_set_expr (&sc->u.expr.dep, NULL);
+			if (sc->u.expr.val) {
+				value_release (sc->u.expr.val);
+				sc->u.expr.val = NULL;
+			}
+		} else if (sc->type != SCT_CONSTRAINT && sc->type != SCT_FLAGS)
+			g_warning ("Unhandled StyleCondition type");
+		
 		g_free (sc);
 	}
 }
 
 /**
  * style_condition_chain:
- * Chain @src to @dst.
+ * Chain @src to @dst with operator @op.
  *
- * Chains are always 'AND' chains, meaning that
- * all the conditions in the chain have to be met.
- * (the chain is evaluated as a whole)
- * 
  * Return value: @dst (the beginning of the chain)
  **/
 StyleCondition *
-style_condition_chain (StyleCondition *dst, StyleCondition *src)
+style_condition_chain (StyleCondition *dst, StyleConditionBool op,
+		       StyleCondition *src)
 {
 	g_return_val_if_fail (dst != NULL, NULL);
 	g_return_val_if_fail (src != NULL, NULL);
 	g_return_val_if_fail (dst->next == NULL, NULL);
 	
-	dst->next = src;
+	dst->next    = src;
+	dst->next_op = op;
 	return dst;
+}
+
+static gboolean
+style_condition_expr_eval (StyleConditionExpr *sce, Value *val, StyleFormat *format)
+{
+	ValueCompare vc;
+			
+	/*
+	 * Apparantly no eval has been done yet, so
+	 * we'll have to force it.
+	 */
+	if (sce->val == NULL) {
+		g_return_val_if_fail (dependent_needs_recalc (&sce->dep), FALSE);
+		dependent_eval (&sce->dep);
+		g_return_val_if_fail (sce->val != NULL, FALSE);
+	}
+
+	vc = value_compare (val, sce->val, TRUE);
+			
+	switch (sce->op) {
+	case SCO_EQUAL :
+		if (vc != IS_EQUAL) return FALSE; break;
+	case SCO_NOT_EQUAL :
+		if (vc == IS_EQUAL) return FALSE; break;
+	case SCO_LESS :
+		if (vc != IS_LESS) return FALSE; break;
+	case SCO_GREATER :
+		if (vc != IS_GREATER) return FALSE; break;
+	case SCO_LESS_EQUAL :
+		if (vc == IS_GREATER) return FALSE; break;
+	case SCO_GREATER_EQUAL :
+		if (vc == IS_LESS) return FALSE; break;
+	default :
+		g_warning ("Style Condition: Unhandled operator"); return FALSE;
+	}
+
+	return TRUE;
+}
+
+static gboolean
+style_condition_constraint_eval (StyleConditionConstraint scc, Value *val, StyleFormat *format)
+{
+	switch (scc) {
+	case SCC_IS_INT :
+		if (val->type != VALUE_BOOLEAN
+		    && val->type != VALUE_INTEGER)
+			return FALSE; break;
+	case SCC_IS_FLOAT :
+		if (val->type != VALUE_FLOAT) return FALSE; break;
+	case SCC_IS_IN_LIST :
+		g_warning ("Style Condition: 'Is In List' not implemented");
+		break;
+	case SCC_IS_DATE :
+		g_warning ("Style Condition: 'In Date' not implemented");
+		break;
+	case SCC_IS_TIME :
+		g_warning ("Style Condition: 'Is Time' not implemented");
+		break;
+	case SCC_IS_TEXTLEN :
+		g_warning ("Style Condition: 'Is Text Length' not implemented");
+		break;
+	default :
+		g_warning ("Style Condition: Unhandled operator");
+		return FALSE;
+	}
+	
+	return TRUE;
+}
+
+static gboolean
+style_condition_flags_eval (StyleConditionFlags scf, Value *val, StyleFormat *format)
+{
+	g_message ("Type %d", val->type);
+	if (val->type == VALUE_EMPTY && (scf & SCF_ALLOW_BLANK))
+		return TRUE;
+	else
+		return FALSE;
+}
+
+void
+style_condition_dump (StyleCondition *sc)
+{
+	StyleCondition *sci;
+	int             i;
+
+	g_return_if_fail (sc != NULL);
+	
+	fprintf (stdout, "---------------------------------------\n");
+	for (i = 0, sci = sc; sci != NULL; sci = sci->next, i++) {
+		char     *t = NULL;
+		GString  *s = NULL;
+		
+		fprintf (stdout, "=> Element %d\n", i);
+
+		switch (sci->type) {
+		case SCT_EXPR       : t = "Expression"; break;
+		case SCT_CONSTRAINT : t = "Constraint"; break;
+		case SCT_FLAGS      : t = "Flags";      break;
+		default :
+			t = "Unknown";
+		}
+		fprintf (stdout, "\tType          : %s\n", t);
+		fprintf (stdout, "\tRef. Count    : %d\n", sci->ref_count);
+
+		switch (sci->type) {
+		case SCT_EXPR :
+			switch (sci->u.expr.op) {
+			case SCO_EQUAL         : t = "== Equal";         break;
+			case SCO_NOT_EQUAL     : t = "!= Not Equal";     break;
+			case SCO_GREATER       : t = ">  Greater";       break;
+			case SCO_LESS          : t = "<  Less";          break;
+			case SCO_GREATER_EQUAL : t = ">= Greater Equal"; break;
+			case SCO_LESS_EQUAL    : t = "<= Less Equal";    break;
+			default :
+				t = "?  Unknown";
+			}
+			fprintf (stdout, "\t\tOperator   : %s\n", t);
+
+			if (sci->u.expr.dep.expression) {
+				ParsePos  pp;
+				Sheet    *sheet = sci->u.expr.dep.sheet;
+				
+				parse_pos_init (&pp, sheet->workbook, sheet, 0, 0);
+				t = expr_tree_as_string (sci->u.expr.dep.expression, &pp);
+				fprintf (stdout, "\t\tExpression : %s\n", t);
+				g_free (t);
+				t = NULL;
+			} else
+				fprintf (stdout, "\t\tExpression : (NULL)\n");
+
+			if (sci->u.expr.val)
+				fprintf (stdout, "\t\tResult     : %s (Cached)\n",
+					 value_peek_string (sci->u.expr.val));
+			else
+				fprintf (stdout, "\t\tResult     : (NULL)\n");
+			
+			break;
+		case SCT_CONSTRAINT :
+			switch (sci->u.constraint) {
+			case SCC_IS_INT     : t = "Whole Number (Integer)"; break;
+			case SCC_IS_FLOAT   : t = "Decimals     (Float)";   break;
+			case SCC_IS_IN_LIST : t = "List         (Region)";  break;
+			case SCC_IS_DATE    : t = "Date         (Integer)"; break;
+			case SCC_IS_TIME    : t = "Time         (Integer)"; break;
+			case SCC_IS_TEXTLEN : t = "TextLength   (Integer)"; break;
+			case SCC_IS_CUSTOM  : t = "Custom       (Any)";     break;
+			default :
+				t = "Unknown      (???)";
+			}
+			fprintf (stdout, "\t\tConstraint : %s\n", t);
+			
+			break;
+		case SCT_FLAGS :
+			s = g_string_new ("");
+
+			if (sci->u.flags & SCF_ALLOW_BLANK)
+				g_string_append (s, "(Allow Blank)");
+			if (sci->u.flags & SCF_IN_CELL_DROPDOWN)
+				g_string_append (s, "(In Cell Dropdown)");
+			
+			fprintf (stdout, "\t\tFlags      : %s\n", s->str);
+			g_string_free (s, TRUE);
+			
+			break;
+		}
+
+		switch (sci->next_op) {
+		case SCB_NONE     : t = "  (None)";     break;
+		case SCB_AND      : t = "& (And)";      break;
+		case SCB_OR       : t = "| (Or)";       break;
+		case SCB_AND_PAIR : t = "& (And PAIR)"; break;
+		case SCB_OR_PAIR  : t = "| (Or PAIR)";  break;
+		default :
+			t = "? (Unknown)";
+		}
+		fprintf (stdout, "\tNext Operator : %s\n\n", t);
+	}
+	fprintf (stdout, "---------------------------------------\n\n");
 }
 
 gboolean
 style_condition_eval (StyleCondition *sc, Value *val, StyleFormat *format)
 {
-	StyleCondition *scl;
-
-	g_return_val_if_fail (val != NULL, FALSE);
+	StyleCondition     *sci;
+	StyleConditionBool  scb = SCB_NONE;
+	gboolean            prev_result = FALSE;
+	gboolean            result = TRUE;
 	
-	for (scl = sc; scl != NULL; scl = scl->next) {
-		if (scl->op >= STYLE_CONDITION_EQUAL &&
-		    scl->op <= STYLE_CONDITION_LESS_EQUAL) {
-			/* Value restriction */
-			ValueCompare vc;
-			
-			/*
-			 * Apparantly no eval has been done yet, so
-			 * we'll have to force it.
-			 */
-			if (scl->val == NULL) {
-				g_return_val_if_fail (dependent_needs_recalc (&scl->dep), FALSE);
-				dependent_eval (&sc->dep);
-				g_return_val_if_fail (scl->val != NULL, FALSE);
-			}
+	g_return_val_if_fail (val != NULL, FALSE);
 
-			vc = value_compare (val, scl->val, TRUE);
-			
-			switch (scl->op) {
-			case STYLE_CONDITION_EQUAL :
-				if (vc != IS_EQUAL) return FALSE; break;
-			case STYLE_CONDITION_NOT_EQUAL :
-				if (vc == IS_EQUAL) return FALSE; break;
-			case STYLE_CONDITION_LESS :
-				if (vc != IS_LESS) return FALSE; break;
-			case STYLE_CONDITION_GREATER :
-				if (vc != IS_GREATER) return FALSE; break;
-			case STYLE_CONDITION_LESS_EQUAL :
-				if (vc == IS_GREATER) return FALSE; break;
-			case STYLE_CONDITION_GREATER_EQUAL :
-				if (vc == IS_LESS) return FALSE; break;
-			default :
-				g_warning ("Style Condition: Unhandled operator"); return FALSE;
-			}
-		} else {
-			/* Type restriction */
-
-			/*
-			 * FIXME: Note all type restrictions are implemented yet
-			 * some of them will need the StyleFormat passed to this
-			 * function
-			 */
-			
-			switch (scl->op) {
-			case STYLE_CONDITION_IS_INT :
-				if (val->type != VALUE_BOOLEAN
-				    && val->type != VALUE_INTEGER)
-					return FALSE; break;
-			case STYLE_CONDITION_IS_FLOAT :
-				if (val->type != VALUE_FLOAT) return FALSE; break;
-			case STYLE_CONDITION_IS_IN_LIST :
-				g_warning ("Style Condition: 'Is In List' not implemented");
-				break;
-			case STYLE_CONDITION_IS_DATE :
-				g_warning ("Style Condition: 'In Date' not implemented");
-				break;
-			case STYLE_CONDITION_IS_TIME :
-				g_warning ("Style Condition: 'Is Time' not implemented");
-				break;
-			case STYLE_CONDITION_IS_TEXTLEN :
-				g_warning ("Style Condition: 'Is Text Length' not implemented");
-				break;
-			default :
-				g_warning ("Style Condition: Unhandled operator"); return FALSE;
-			}
+	for (sci = sc; sci != NULL; sci = sci->next) {
+		result = TRUE;
+		
+		switch (sci->type) {
+		case SCT_EXPR :
+			result = style_condition_expr_eval (&sci->u.expr, val, format);
+			break;
+		case SCT_CONSTRAINT :
+			result = style_condition_constraint_eval (sci->u.constraint, val, format);
+			break;
+		case SCT_FLAGS :
+			result = style_condition_flags_eval (sci->u.flags, val, format);
+			break;
+		default :
+			g_warning ("Unknown StyleCondition type");
 		}
+
+		/*
+		 * NOTE: Pay careful attention to the four chain operators.
+		 *
+		 * The normal two, SCB_AND and SCB_OR, just evaluate things as list.
+		 * Example : X SCB_AND Y SCB_OR Z
+		 *           (first does "X & Y" then does "Y | Z")
+		 * Observe that none of the operators take precedence over the other.
+		 *
+		 * The other two, SCB_AND_PAIR and SCB_OR_PAIR explicitely indicate
+		 * precendence by using 'parens'.
+		 * Example : X SCB_OR_PAIR Y SCB_AND_PAIR Z
+		 *           (can be written as : "((X | Y) & Z)")
+		 *
+		 * And a combination  : X SCB_OR_PAIR Y SCB_AND_PAIR Z SCB_OR J
+		 * Which evaluates to : ((X | Y) & Z) | J
+		 */
+		switch (scb) {
+		case SCB_AND :
+			if (!(result && prev_result))
+				return FALSE;
+			else
+				result = TRUE;
+			break;
+		case SCB_OR :
+			if (!(result || prev_result))
+				return FALSE;
+			else
+				result = TRUE;
+			break;
+		case SCB_AND_PAIR :
+			result = (result && prev_result);
+			break;
+		case SCB_OR_PAIR :
+			result = (result || prev_result);
+			break;
+		case SCB_NONE :
+			if (sci != sc && sci->next != NULL)
+				g_warning ("Corrupted StyleCondition Chain");
+			break;
+		default :
+			g_warning ("Unknown StyleCondition Chain Operator");
+		}
+
+		scb = sci->next_op;
+		prev_result = result;
 	}
 	
-	return TRUE;
+	return result;
 }
--- src/style-condition.h
+++ src/style-condition.h
@@ -10,40 +10,80 @@
 typedef enum _ValidationStyle {
 	VALIDATION_STYLE_NONE,
 	VALIDATION_STYLE_STOP,
-	VALIDATION_STYLE_ERROR,
-	VALIDATION_STYLE_WARNING
+	VALIDATION_STYLE_WARNING,
+	VALIDATION_STYLE_INFO
 } ValidationStyle;
 
-typedef enum _StyleConditionOp {
-	STYLE_CONDITION_EQUAL,
-	STYLE_CONDITION_NOT_EQUAL,
-	STYLE_CONDITION_GREATER,
-	STYLE_CONDITION_LESS,
-	STYLE_CONDITION_GREATER_EQUAL,
-	STYLE_CONDITION_LESS_EQUAL,
-	
-	STYLE_CONDITION_IS_INT,
-	STYLE_CONDITION_IS_FLOAT,
-	STYLE_CONDITION_IS_IN_LIST,
-	STYLE_CONDITION_IS_DATE,
-	STYLE_CONDITION_IS_TIME,
-	STYLE_CONDITION_IS_TEXTLEN
-} StyleConditionOp;
+typedef enum {
+	SCT_EXPR        = 10,
+	SCT_CONSTRAINT  = 20,
+	SCT_FLAGS       = 30
+} StyleConditionType;
 
-struct _StyleCondition {
-	int                     ref_count;
-	StyleConditionOp        op;
+typedef enum {
+	SCB_NONE,
+	SCB_AND,
+	SCB_OR,
+	SCB_AND_PAIR,
+	SCB_OR_PAIR
+} StyleConditionBool;
+
+typedef enum {
+	SCO_EQUAL,
+	SCO_NOT_EQUAL,
+	SCO_GREATER,
+	SCO_LESS,
+	SCO_GREATER_EQUAL,
+	SCO_LESS_EQUAL,
+	SCO_LAST
+} StyleConditionOperator;
+
+typedef struct {
+	StyleConditionOperator  op;
 	Dependent               dep;
 	Value                  *val;
+} StyleConditionExpr;
 
+typedef enum {
+	SCC_IS_INT,
+	SCC_IS_FLOAT,
+	SCC_IS_IN_LIST,
+	SCC_IS_DATE,
+	SCC_IS_TIME,
+	SCC_IS_TEXTLEN,
+	SCC_IS_CUSTOM
+} StyleConditionConstraint;
+
+typedef enum {
+	SCF_ALLOW_BLANK      = 1 << 0,
+	SCF_IN_CELL_DROPDOWN = 1 << 1
+} StyleConditionFlags;
+
+struct _StyleCondition {
+	StyleConditionType type;
+	int                ref_count;
+
+	union {
+		StyleConditionExpr       expr;
+		StyleConditionConstraint constraint;
+		StyleConditionFlags      flags;
+	} u;
+	
 	struct _StyleCondition *next;
+	StyleConditionBool      next_op;
 };
 
-StyleCondition *style_condition_new   (Sheet *sheet, StyleConditionOp op, ExprTree *expr);
+StyleCondition *style_condition_new_expr       (Sheet *sheet, StyleConditionOperator op,
+						ExprTree *expr);
+StyleCondition *style_condition_new_constraint (StyleConditionConstraint constraint);
+StyleCondition *style_condition_new_flags      (StyleConditionFlags flags);
+
 void            style_condition_ref   (StyleCondition *sc);
 void            style_condition_unref (StyleCondition *sc);
 
-StyleCondition *style_condition_chain (StyleCondition *dst, StyleCondition *src);
+void            style_condition_dump  (StyleCondition *sc);
+StyleCondition *style_condition_chain (StyleCondition *dst, StyleConditionBool op,
+				       StyleCondition *src);
 gboolean        style_condition_eval  (StyleCondition *sc, Value *val, StyleFormat *format);
 
 #endif /* GNUMERIC_STYLE_VALIDATION_H */
