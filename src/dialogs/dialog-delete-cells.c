d6dfd54bf0de4cc3fb7d654b26164df9f14f46a1
Andreas J. Guelzow <aguelzow@taliesin.ca>
2002-02-28 18:11:07 +0000

    add help button, change some ids rewrite as guru
    2002-02-28  Andreas J. Guelzow <aguelzow@taliesin.ca>
    
    	* delete-cells.glade : add help button, change some ids
    	* dialog-delete-cells.c : rewrite as guru

--- src/dialogs/dialog-delete-cells.c
+++ src/dialogs/dialog-delete-cells.c
@@ -1,8 +1,23 @@
 /*
- * dialog-delete-cells.c: Delete a number of cells.
+ * dialog-insert-cells.c: Insert a number of cells.
  *
- * Author:
- *  Miguel de Icaza (miguel@gnu.org)
+ * Authors:
+ *                Miguel de Icaza (miguel@gnu.org)
+ * Copyright (C)  Andreas J. Guelzow (aguelzow@taliesin.ca)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 #include <gnumeric-config.h>
 #include <gnumeric.h>
@@ -15,71 +30,104 @@
 #include <commands.h>
 #include <ranges.h>
 #include <cmd-edit.h>
+#include <workbook-edit.h>
 
 #include <libgnome/gnome-i18n.h>
 #include <glade/glade.h>
 
 #define GLADE_FILE "delete-cells.glade"
+#define DELETE_CELL_DIALOG_KEY "delete-cells-dialog"
+
+typedef struct {
+	WorkbookControlGUI *wbcg;
+	GtkWidget          *dialog;
+	GtkWidget          *ok_button;
+	GtkWidget          *cancel_button;
+	Range const        *sel;
+	Sheet              *sheet;
+	GladeXML           *gui;
+} DeleteCellState;
+
+static gboolean
+delete_cell_destroy (GtkObject *w, DeleteCellState *state)
+{
+	g_return_val_if_fail (w != NULL, FALSE);
+	g_return_val_if_fail (state != NULL, FALSE);
+
+	wbcg_edit_detach_guru (state->wbcg);
+
+	if (state->gui != NULL) {
+		g_object_unref (G_OBJECT (state->gui));
+		state->gui = NULL;
+	}
+
+	state->dialog = NULL;
+	g_free (state);
+
+	return FALSE;
+}
 
 static void
-dialog_delete_cells_impl (WorkbookControlGUI *wbcg, GladeXML *gui,
-			  Sheet *sheet, Range const *sel)
+cb_delete_cell_ok_clicked (GtkWidget *button, DeleteCellState *state)
 {
-	WorkbookControl *wbc = WORKBOOK_CONTROL (wbcg);
-	GtkWidget *dialog, *radio_0;
+	WorkbookControl *wbc = WORKBOOK_CONTROL (state->wbcg);
+	GtkWidget *radio_0;
 	int  cols, rows;
-	int i, res;
+	int i;
 
-	dialog = glade_xml_get_widget (gui, "Delete_cells");
-	if (dialog == NULL) {
-		g_print ("Corrupt file " GLADE_FILE "\n");
-		return;
-	}
-	radio_0 = glade_xml_get_widget (gui, "radio_0");
+	radio_0 = glade_xml_get_widget (state->gui, "radio_0");
 	g_return_if_fail (radio_0 != NULL);
 
-	/* Bring up the dialog */
-	res = gnumeric_dialog_run (wbcg, GTK_DIALOG (dialog));
-	if (res == GNOME_OK) {
-		i = gtk_radio_group_get_selected
-			(GTK_RADIO_BUTTON (radio_0)->group);
+	i = gtk_radio_group_get_selected
+		(GTK_RADIO_BUTTON (radio_0)->group);
+	
+	cols = state->sel->end.col - state->sel->start.col + 1;
+	rows = state->sel->end.row - state->sel->start.row + 1;
 
-		cols = sel->end.col - sel->start.col + 1;
-		rows = sel->end.row - sel->start.row + 1;
-
-		if (i == 0)
-			cmd_shift_rows (wbc, sheet,
-					sel->start.col + cols,
-					sel->start.row,
-					sel->end.row, -cols);
-		else if (i == 1)
-			cmd_shift_cols (wbc, sheet,
-					sel->start.col,
-					sel->end.col,
-					sel->start.row + rows, -rows);
-		else if (i == 2)
-			cmd_delete_rows (wbc, sheet, sel->start.row, rows);
-		else if (i == 3)
-			cmd_delete_cols (wbc, sheet, sel->start.col, cols);
+	switch (i) {
+	case 0 :
+		cmd_shift_rows (wbc, state->sheet,
+				state->sel->end.col + 1,
+				state->sel->start.row,
+				state->sel->end.row, - cols);
+		break;
+	case 1 :
+		cmd_shift_cols (wbc, state->sheet,
+				state->sel->start.col,
+				state->sel->end.col,
+				state->sel->end.row + 1, - rows);
+		break;
+	case 2 :
+		cmd_delete_rows (wbc, state->sheet,
+				 state->sel->start.row, rows);
+		break;
+	default :
+		cmd_delete_cols (wbc, state->sheet,
+				 state->sel->start.col, cols);
+		break;
 	}
+	gtk_widget_destroy (state->dialog);
+}
 
-	/* If user closed the dialog with prejudice, it's already destroyed */
-	if (res >= 0)
-		gnome_dialog_close (GNOME_DIALOG (dialog));
+static void
+cb_delete_cell_cancel_clicked (GtkWidget *button, DeleteCellState *state)
+{
+	gtk_widget_destroy (state->dialog);
+	return;
 }
 
 void
 dialog_delete_cells (WorkbookControlGUI *wbcg, Sheet *sheet)
 {
+	DeleteCellState *state;
 	WorkbookControl *wbc = WORKBOOK_CONTROL (wbcg);
-	GladeXML  *gui;
 	Range const *sel;
 	int  cols, rows;
 
 	g_return_if_fail (wbcg != NULL);
 	g_return_if_fail (IS_SHEET (sheet));
 
-	if (!(sel = selection_first_range (sheet, wbc, _("Delete cells"))))
+	if (!(sel = selection_first_range (sheet, wbc, _("Delete"))))
 		return;
 	cols = sel->end.col - sel->start.col + 1;
 	rows = sel->end.row - sel->start.row + 1;
@@ -93,12 +141,45 @@ dialog_delete_cells (WorkbookControlGUI *wbcg, Sheet *sheet)
 		return;
 	}
 
-	gui = gnumeric_glade_xml_new (wbcg, GLADE_FILE);
-        if (gui == NULL)
-                return;
+	if (gnumeric_dialog_raise_if_exists (wbcg, DELETE_CELL_DIALOG_KEY))
+		return;
 
-	/* Wrapper to ensure the libglade object gets removed on error */
-	dialog_delete_cells_impl (wbcg, gui, sheet, sel);
+	state = g_new (DeleteCellState, 1);
+	state->wbcg  = wbcg;
+	state->sel   = sel;
+	state->sheet = sheet;
 
-	g_object_unref (G_OBJECT (gui));
+	state->gui = gnumeric_glade_xml_new (wbcg, GLADE_FILE);
+	g_return_if_fail (state->gui != NULL);
+
+	state->dialog = glade_xml_get_widget (state->gui, "Delete_cells");
+	if (state->dialog == NULL) {
+		gnumeric_notice (wbcg, GTK_MESSAGE_ERROR,
+				 _("Could not create the Delete Cell dialog."));
+		g_free (state);
+		return ;
+	}
+
+	state->ok_button = glade_xml_get_widget (state->gui, "okbutton");
+	g_signal_connect (G_OBJECT (state->ok_button),
+		"clicked",
+		G_CALLBACK (cb_delete_cell_ok_clicked), state);
+
+	state->cancel_button = glade_xml_get_widget (state->gui, "cancelbutton");
+	g_signal_connect (G_OBJECT (state->cancel_button),
+		"clicked",
+		G_CALLBACK (cb_delete_cell_cancel_clicked), state);
+/* FIXME: Add correct helpfile address */
+	gnumeric_init_help_button (
+		glade_xml_get_widget (state->gui, "helpbutton"),
+		"delete-cells.html");
+
+	g_signal_connect (G_OBJECT (state->dialog),
+		"destroy",
+		G_CALLBACK (delete_cell_destroy), state);
+
+	wbcg_edit_attach_guru (state->wbcg, state->dialog);
+	gnumeric_keyed_dialog (wbcg, GTK_WINDOW (state->dialog),
+			       DELETE_CELL_DIALOG_KEY);
+	gtk_widget_show (state->dialog);
 }
