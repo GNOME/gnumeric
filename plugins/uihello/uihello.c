9fe39f9cb10eccc197131b6a3ebc978932d4e9bf
Zbigniew Chyla <cyba@gnome.pl>
2002-08-07 10:07:52 +0000

    Initial version of the plugin.
    2002-08-05  Zbigniew Chyla  <cyba@gnome.pl>
    
    	* plugin.xml.in, uihello.c, Makefile.am, ChangeLog, hello.xml:
    	Initial version of the plugin.

---
+++ plugins/uihello/uihello.c
@@ -0,0 +1,35 @@
+/*
+ * uihello.c: sample plugin using "ui" service
+ *
+ * Author: Zbigniew Chyla (cyba@gnome.pl)
+ */
+
+#include <gnumeric-config.h>
+#include <glib.h>
+#include <libgnome/gnome-i18n.h>
+
+#include <workbook-control-gui.h>
+#include <gui-util.h>
+#include <plugin.h>
+#include <module-plugin-defs.h>
+
+GNUMERIC_MODULE_PLUGIN_INFO_DECL;
+
+#define PLUGIN (plugins_get_plugin_by_id ("Gnumeric_UIHello"))
+
+static void
+hello_message (WorkbookControlGUI *wbcg)
+{
+	char *msg;
+
+	msg = g_strdup_printf (
+		_("This is message from \"%s\" plugin."),
+		gnm_plugin_get_name (PLUGIN));
+	gnumeric_notice (wbcg, GTK_MESSAGE_INFO, msg);
+	g_free (msg);
+}
+
+const ModulePluginUIVerbInfo hello_ui_verbs[] = {
+	{"HelloWorld", hello_message},
+	{NULL}
+};
