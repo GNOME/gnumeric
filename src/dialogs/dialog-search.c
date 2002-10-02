20a1ac26e92916a62de4137f5bc3a5c92bdcb319
Morten Welinder <mortenw@src.gnome.org>
2002-10-02 15:41:23 +0000

    Stealth fix.

--- src/dialogs/dialog-search.c
+++ src/dialogs/dialog-search.c
@@ -363,7 +363,6 @@ make_matches_table (GtkTreeModel *model)
 			gtk_tree_view_column_new_with_attributes (_(columns[i].title), cell,
 								  columns[i].type, i,
 								  NULL);
-		gtk_tree_view_column_set_sort_column_id (column, i);
 		gtk_tree_view_append_column (tree_view, column);
 	}
 
