1a35866091345c5cbb8650a26e2e0996b9fefcb9
Morten Welinder <mortenw@src.gnome.org>
2002-11-26 16:08:01 +0000

    Part II...

--- src/widgets/gnumeric-expr-entry.c
+++ src/widgets/gnumeric-expr-entry.c
@@ -1182,7 +1182,7 @@ gnm_expr_entry_parse (GnumericExprEntry *gee, ParsePos const *pp,
 	if (!(gee->flags & GNUM_EE_SHEET_OPTIONAL))
 		flags |= GNM_EXPR_PARSE_FORCE_EXPLICIT_SHEET_REFERENCES;
 
-	expr = gnm_expr_parse_str (text, pp, flags, &rangeref_parse, perr);
+	expr = gnm_expr_parse_str (text, pp, flags, gnm_expr_conventions_default, perr);
 	if (expr == NULL)
 		return NULL;
 
