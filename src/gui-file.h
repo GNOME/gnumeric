a0f52201d1585482e6d10ae9dadc0d3fee952bb8
Jody Goldberg <jody@gnome.org>
2003-01-28 07:15:17 +0000

    Release 1.1.16
    2003-01-28  Jody Goldberg <jody@gnome.org>
    
    	* Release 1.1.16

--- src/gui-file.h
+++ src/gui-file.h
@@ -2,20 +2,11 @@
 #define GNUMERIC_GUI_FILE_H
 
 #include "gui-gnumeric.h"
-#ifdef WITH_BONOBO
-#include <bonobo/bonobo-stream.h>
-#endif
 
 gboolean gui_file_save_as   (WorkbookControlGUI *wbcg, WorkbookView *);
 gboolean gui_file_save      (WorkbookControlGUI *wbcg, WorkbookView *);
 void     gui_file_open      (WorkbookControlGUI *wbcg);
 gboolean gui_file_read	    (WorkbookControlGUI *wbcg, char const *file_name,
 			     GnmFileOpener const *optional_format);
-
-#ifdef WITH_BONOBO
-void gui_file_save_to_stream (BonoboStream *stream, WorkbookControlGUI *wbcg,
-		              WorkbookView *wb_view, const gchar *mime_type,
-			      CORBA_Environment *ev);
-#endif
 
 #endif /* GNUMERIC_GUI_FILE_H */
