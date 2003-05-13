d1433d8b96a451c1af001b6ccfb4a42440c2fa7e
Morten Welinder <mortenw@src.gnome.org>
2003-05-13 21:21:29 +0000

    Eliminate debug code.

--- src/widgets/widget-charmap-selector.c
+++ src/widgets/widget-charmap-selector.c
@@ -152,7 +152,8 @@ CharsetInfo const charset_trans_array[] = {
 	{N_("Turkisk (Windows-1254)"),            "windows-1254",          LG_TURKISH},
 	{N_("Unicode (UTF-7)"),                   "UTF-7",                 LG_UNICODE},
 	{N_("Unicode (UTF-8)"),                   "UTF-8",                 LG_UNICODE},
-	{N_("Unicode (UTF-9)"),                   "UTF-9",                 LG_UNICODE},
+	/* Test encoding that surely does not exist.  */
+	/* {"Unicode (UTF-9)",                    "UTF-9",                 LG_UNICODE}, */
 	{N_("Unicode (UTF-16BE)"),                "UTF-16BE",              LG_UNICODE},
 	{N_("Unicode (UTF-16LE)"),                "UTF-16LE",              LG_UNICODE},
 	{N_("Unicode (UTF-32BE)"),                "UTF-32BE",              LG_UNICODE},
