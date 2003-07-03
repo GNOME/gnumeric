d6ff066f9f340a7d15b26caa99b7ac8f2723a665
Jody Goldberg <jody@src.gnome.org>
2003-07-03 16:19:25 +0000

    update

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,7 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="Gnumeric"
+REQUIRED_LIBTOOL_VERSION=1.4.3
 
 (test -f $srcdir/configure.in \
   && test -d $srcdir/src \
@@ -13,16 +14,6 @@ PKG_NAME="Gnumeric"
     echo " top-level gnumeric directory"
     exit 1
 }
-
-if libtool --version >/dev/null 2>&1; then
-    vers=`libtool --version | sed -e "s/^[^0-9]*//" -e "s/ .*$//" | awk 'BEGIN { FS = "."; } { printf "%d", ($1 * 1000 + $2) * 1000 + $3;}'`
-    if test "$vers" -ge 1003003; then
-        true
-    else
-        echo "Please upgrade your libtool to version 1.3.3 or better." 1>&2
-        exit 1
-    fi
-fi
 
 ifs_save="$IFS"; IFS=":"
 for dir in $PATH ; do
