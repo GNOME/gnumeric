1b889a788e655d7f35eb612e2c0624df69184a55
Rodrigo Moya <rodrigo@gnome-db.org>
2003-07-06 13:52:10 +0000

    Started gnome-db-based plugin.
    2003-07-06  Rodrigo Moya <rodrigo@gnome-db.org>
    
    	Started gnome-db-based plugin.
    
    	* plugin-gnomedb.c: UI plugin, containing the Tools/Data Sources menu
    	item.
    
    	* plugin.xml.in: plugin information file.
    
    	* ui.xml: UI description file.

---
+++ plugins/gnome-db/plugin-gnomedb.c
@@ -0,0 +1,30 @@
+
+#include <gnumeric-config.h>
+#include <gnumeric-i18n.h>
+#include <glib.h>
+
+#include <workbook-control-gui.h>
+#include <gui-util.h>
+#include <plugin.h>
+#include <module-plugin-defs.h>
+
+GNUMERIC_MODULE_PLUGIN_INFO_DECL;
+
+static void
+view_data_sources (WorkbookControlGUI *wbcg)
+{
+	char *argv[2];
+
+	/* run gnome-database-properties config tool */
+	argv[0] = (char *) "gnome-database-properties";
+	argv[1] = NULL;
+
+	if (!g_spawn_async (NULL, argv, NULL, G_SPAWN_SEARCH_PATH,
+			    NULL, NULL, NULL, NULL))
+		gnumeric_notice (wbcg, GTK_MESSAGE_INFO, _("Could not run GNOME database configuration tool"));
+}
+
+const ModulePluginUIVerbInfo gnome_db_ui_verbs[] = {
+	{"ViewDataSources", view_data_sources},
+	{NULL}
+};
