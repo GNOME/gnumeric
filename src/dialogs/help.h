161e3981376b4a620cc17cf9c968dab7cbbad2ba
Andreas J. Guelzow <aguelzow@taliesin.ca>
2003-08-09 06:32:01 +0000

    new, list of help links for all help buttons replace helpfile address with
    2003-08-09  Andreas J. Guelzow  <aguelzow@taliesin.ca>
    
    	* help.h: new, list of help links for all help buttons
    	* dialog-advanced-filter.c: replace helpfile address
    	  with #define from help.h
    	* dialog-autocorrect.c: ditto
    	* dialog-autofilter.c: ditto
    	* dialog-autoformat.c: ditto
    	* dialog-cell-comment.c: ditto
    	* dialog-cell-format.c: ditto
    	* dialog-cell-sort.c: ditto
    	* dialog-col-row.c: ditto
    	* dialog-col-row.c: ditto
    	* dialog-col-width.c: ditto
    	* dialog-consolidate.c: ditto
    	* dialog-consolidate.c: ditto
    	* dialog-define-names.c: ditto
    	* dialog-define-names.c: ditto
    	* dialog-delete-cells.c: ditto
    	* dialog-fill-series.c: ditto
    	* dialog-formula-guru.c: ditto
    	* dialog-function-select.c: ditto
    	* dialog-goal-seek.c: ditto
    	* dialog-goto-cell.c: ditto
    	* dialog-hyperlink.c: ditto
    	* dialog-insert-cells.c: ditto
    	* dialog-merge.c: ditto
    	* dialog-pivottable.c: ditto
    	* dialog-plugin-manager.c: ditto
    	* dialog-preferences.c: ditto
    	* dialog-printer-setup.c: ditto
    	* dialog-random-generator.c: ditto
    	* dialog-row-height.c: ditto
    	* dialog-scenarios.c: ditto
    	* dialog-search-replace.c: ditto
    	* dialog-search.c: ditto
    	* dialog-sheet-order.c: ditto
    	* dialog-shuffle.c: ditto
    	* dialog-simulation.c: ditto
    	* dialog-solver.c: ditto
    	* dialog-summary.c: ditto
    	* dialog-tabulate.c: ditto
    	* dialog-view.c: ditto
    	* dialog-workbook-attr.c: ditto
    	* dialog-zoom.c: ditto
    	* dialog-analysis-tools.c: ditto, and remove some
    	  duplicate code
    	* dialog-autosave.c: ditto, and use utilities

---
+++ src/dialogs/help.h
@@ -0,0 +1,173 @@
+/* vim: set sw=8: -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#ifndef GNUMERIC_HELP_H
+#define GNUMERIC_HELP_H
+
+/* ../sheet-object-graphic.c   */
+#define GNUMERIC_HELP_LINK_SO_LINE ""
+#define GNUMERIC_HELP_LINK_SO_ARROW ""
+
+/* ../sheet-object-graphic.c   */
+#define GNUMERIC_HELP_LINK_SO_FILLED ""
+
+/* ../sheet-object-widget.c    */
+#define GNUMERIC_HELP_LINK_SO_FRAME ""
+#define GNUMERIC_HELP_LINK_SO_ADJUSTMENT ""
+#define GNUMERIC_HELP_LINK_SO_CHECKBOX ""
+
+/* dialog-autocorrect.c        */
+#define GNUMERIC_HELP_LINK_AUTOCORRECT "sect-data-entry"
+
+/* dialog-autofilter.c         */
+#define GNUMERIC_HELP_LINK_AUTOFILTER ""
+
+/* dialog-autoformat.c         */
+#define GNUMERIC_HELP_LINK_AUTOFORMAT "sect-data-entry"
+
+/* dialog-autosave.c         */
+#define GNUMERIC_HELP_LINK_AUTOSAVE "sect-files-autosave"
+
+/* dialog-cell-comment.c       */
+#define GNUMERIC_HELP_LINK_CELL_COMMENT "sect-data-comment"
+
+/* dialog-cell-format.c       */
+#define GNUMERIC_HELP_LINK_CELL_FORMAT "sect-data-format"
+
+/* dialog-cell-sort.c  */
+#define GNUMERIC_HELP_LINK_CELL_SORT "sect-data-modify"
+
+/* dialog-col-row.c    */
+#define GNUMERIC_HELP_LINK_GROUP_UNGROUP "sect-worksheets-viewing"
+
+/* dialog-col-width.c  */
+#define GNUMERIC_HELP_LINK_COL_WIDTH "sect-worksheets-colrow"
+
+/* dialog-define-names.c       */
+#define GNUMERIC_HELP_LINK_DEFINE_NAMES "sect-workbooks-names"
+
+/* dialog-delete-cells.c       */
+#define GNUMERIC_HELP_LINK_DELETE_CELLS "sect-data-delete"
+
+/* dialog-formula-guru.c       */
+#define GNUMERIC_HELP_LINK_FORMULA_GURU "sect-data-entry"
+
+/* dialog-function-select.c    */
+#define GNUMERIC_HELP_LINK_FUNCTION_SELECT "sect-data-entry"
+
+/* dialog-goal-seek.c  */
+#define GNUMERIC_HELP_LINK_GOAL_SEEK "sect-analysis-goalseek"
+
+/* dialog-goto-cell.c  */
+#define GNUMERIC_HELP_LINK_GOTO_CELL "sect-worksheets-viewing"
+
+/* dialog-hyperlink.c  */
+#define GNUMERIC_HELP_LINK_HYPERLINK "sect-data-link"
+
+/* dialog-insert-cells.c */
+#define GNUMERIC_HELP_LINK_INSERT_CELS "sect-data-insert"
+
+/* dialog-merge.c      */
+#define GNUMERIC_HELP_LINK_DATA_MERGE "sect-data-generate"
+
+/* dialog-pivottable.c         */
+#define GNUMERIC_HELP_LINK_PIVOT_TABLE ""
+
+/* dialog-plugin-manager.c     */
+#define GNUMERIC_HELP_LINK_PLUGIN_MANAGER "sect-configuring-plugins"
+
+/* dialog-preferences.c       */
+#define GNUMERIC_HELP_LINK_PREFERENCES "sect-configuration-preferences"
+
+/* dialog-printer-setup.c     */
+#define GNUMERIC_HELP_LINK_PRINTER_SETUP_HEADER_CUSTOMIZATION "sect-printing-setup"
+#define GNUMERIC_HELP_LINK_PRINTER_SETUP_FOOTER_CUSTOMIZATION "sect-printing-setup"
+#define GNUMERIC_HELP_LINK_PRINTER_SETUP_GENERAL "sect-printing-setup"
+
+/* dialog-row-height.c         */
+#define GNUMERIC_HELP_LINK_ROW_HEIGHT "sect-worksheets-colrow"
+
+/* dialog-search-replace.c     */
+#define GNUMERIC_HELP_LINK_SEARCH_REPLACE "sect-data-modify"
+
+/* dialog-search.c     */
+#define GNUMERIC_HELP_LINK_SEARCH "sect-data-modify"
+
+/* dialog-sheet-order.c       */
+#define GNUMERIC_HELP_LINK_SHEET_MANAGER "sect-worksheets-managing"
+
+/* dialog-solver.c    */
+#define GNUMERIC_HELP_LINK_SOLVER "sect-analysis-solver"
+
+/* dialog-summary.c   */
+#define GNUMERIC_HELP_LINK_SUMMARY "sect-workbooks-docsummary"
+
+/* dialog-tabulate.c  */
+#define GNUMERIC_HELP_LINK_TABULATE "sect-data-generate"
+
+/* dialog-view.c      */
+#define GNUMERIC_HELP_LINK_VIEW "sect-worksheets-viewing"
+
+/* dialog-workbook-attr.c */
+#define GNUMERIC_HELP_LINK_WORKBOOK_ATTRIBUTE "sect-workbooks"
+
+/* dialog-zoom.c      */
+#define GNUMERIC_HELP_LINK_ZOOM "sect-worksheets-viewing"
+
+/* dialog-advanced-filter.c  */
+#define GNUMERIC_HELP_LINK_ADVANCED_FILTER "sect-data-modify"
+
+/* dialog-analysis-tools.c */
+#define GNUMERIC_HELP_LINK_CORRELATION "sect-analysis-statistical"
+#define GNUMERIC_HELP_LINK_COVARIANCE "sect-analysis-statistical"
+#define GNUMERIC_HELP_LINK_RANKING "sect-analysis-statistical"
+#define GNUMERIC_HELP_LINK_FOURIER_ANALYSIS "sect-analysis-statistical"
+#define GNUMERIC_HELP_LINK_DESCRIPTIVE_STATS "sect-analysis-statistical"
+#define GNUMERIC_HELP_LINK_MEAN_TESTS "sect-analysis-statistical"
+#define GNUMERIC_HELP_LINK_F_TEST_TWO_SAMPLE "sect-analysis-statistical"
+#define GNUMERIC_HELP_LINK_SAMPLING "sect-analysis-statistical"
+#define GNUMERIC_HELP_LINK_REGRESSION "sect-analysis-statistical"
+#define GNUMERIC_HELP_LINK_EXP_SMOOTHING "sect-analysis-statistical"
+#define GNUMERIC_HELP_LINK_MOVING_AVERAGES "sect-analysis-statistical"
+#define GNUMERIC_HELP_LINK_HISTOGRAM "sect-analysis-statistical"
+#define GNUMERIC_HELP_LINK_ANOVA_SINGLE_FACTOR "sect-analysis-statistical"
+#define GNUMERIC_HELP_LINK_ANOVA_TWO_FACTOR "sect-analysis-statistical"
+
+/* dialog-consolidate.c     */
+#define GNUMERIC_HELP_LINK_CONSOLIDATE ""
+
+/* dialog-fill-series.c     */
+#define GNUMERIC_HELP_LINK_FILL_SERIES "sect-data-entry"
+
+/* dialog-random-generator.c */
+#define GNUMERIC_HELP_LINK_RANDOM_GENERATOR "sect-dataentryadv"
+
+/* dialog-scenarios.c */
+#define GNUMERIC_HELP_LINK_SCENARIOS_ADD ""
+#define GNUMERIC_HELP_LINK_SCENARIOS_VIEW ""
+
+/* dialog-shuffle.c   */
+#define GNUMERIC_HELP_LINK_DATA_SHUFFLE "sect-data-modify"
+
+/* dialog-simulation.c */
+#define GNUMERIC_HELP_LINK_SIMULATION "sect-analysis-simulation"
+
+
+/**********************************************************************/
+/* The following links are included directly in the source code file: */
+/* ../cut-n-paste-code/goffice/graph/gog-guru.c  */
+
+#endif
