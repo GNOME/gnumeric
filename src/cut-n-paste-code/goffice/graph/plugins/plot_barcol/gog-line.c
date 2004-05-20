3cf01ed690282645f657abb6268b0b34ed393f10
Emmanuel Pacaud <emmanuel.pacaud@univ-poitiers.fr>
2004-05-20 21:59:42 +0000

    http://bugzilla.gnome.org/show_bug.cgi?id=142212 don't use '0' if Y value
    2004-05-20  Emmanuel Pacaud <emmanuel.pacaud@univ-poitiers.fr>
    	http://bugzilla.gnome.org/show_bug.cgi?id=142212
    	* graph/plugins/plot-barcol/gog-line.c (gog_line_view_render) : don't
    	use '0' if Y value is missing.

--- src/cut-n-paste-code/goffice/graph/plugins/plot_barcol/gog-line.c
+++ src/cut-n-paste-code/goffice/graph/plugins/plot_barcol/gog-line.c
@@ -289,7 +289,6 @@ gog_line_view_render (GogView *view, GogViewAllocation const *bbox)
 
 	for (j = 1; j <= num_elements; j++) {
 		sum = abs_sum = 0.0;
-
 		if (type == GOG_1_5D_AS_PERCENTAGE) {
 			for (i = 0; i < num_series; i++)
 				if (finitegnum (vals[i][j-1]))
@@ -316,7 +315,16 @@ gog_line_view_render (GogView *view, GogViewAllocation const *bbox)
 			}
 
 			path[i][j].x = offset_x + scale_x * (j - 1);
-			path[i][j].code = ART_LINETO;
+			if (type == GOG_1_5D_NORMAL && !is_area_plot) 
+				if (finite (vals[i][j-1])) 
+					if (j > 1 && path[i][j-1].code == ART_MOVETO_OPEN)
+						path[i][j].code = ART_MOVETO;
+					else
+						path[i][j].code = ART_LINETO;
+				else
+					path[i][j].code = ART_MOVETO_OPEN;
+			else
+				path[i][j].code = ART_LINETO;
 
 			sum += value;
 
@@ -333,7 +341,6 @@ gog_line_view_render (GogView *view, GogViewAllocation const *bbox)
 					path[i][j].y = is_null ? offset_y : offset_y + scale_y * sum  / abs_sum;
 					break;
 			}
-
 		}
 	}
 
@@ -362,7 +369,8 @@ gog_line_view_render (GogView *view, GogViewAllocation const *bbox)
 			for (j = 1; j <= lengths[i]; j++) {
 				x = path[i][j].x;
 				y = path[i][j].y;
-				if (min_x <= x && x <= max_x && min_y <= y && y <= max_y)
+				if (min_x <= x && x <= max_x && min_y <= y && y <= max_y &&
+				    path[i][j].code != ART_MOVETO_OPEN)
 					gog_renderer_draw_marker (view->renderer, x, y);
 			}
 		} else {
