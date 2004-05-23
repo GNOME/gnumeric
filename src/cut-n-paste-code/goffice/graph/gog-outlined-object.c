483750d1a378d4f179333e3ab457c635e248d257
Emmanuel Pacaud <emmanuel.pacaud@univ-poitiers.fr>
2004-05-23 07:40:59 +0000

    set call_parent_render to FALSE. call parent render method only if
    2004-05-23  Emmanuel Pacaud <emmanuel.pacaud@univ-poitiers.fr>
    
    	* graph/gog-chart.c (gog_chart_view_class_init) : set
    call_parent_render
    	to FALSE.
    	* graph/gog-outlined-object.c (gog_outlined_view_render) : call
    parent
    	render method only if call_parent_render is TRUE;
    	(gog_outlined_view_class_init) : set call_parent_render to TRUE;
    	* graph/gog-outlined-object.h : add a call_parent_render
    boolean.

--- src/cut-n-paste-code/goffice/graph/gog-outlined-object.c
+++ src/cut-n-paste-code/goffice/graph/gog-outlined-object.c
@@ -140,20 +140,28 @@ gog_outlined_view_size_allocate (GogView *v, GogViewAllocation const *a)
 static void
 gog_outlined_view_render (GogView *view, GogViewAllocation const *bbox)
 {
+	GogOutlinedViewClass *klass = GOG_OUTLINED_VIEW_GET_CLASS (view);
+
 	GogStyledObject *sobj = GOG_STYLED_OBJECT (view->model);
 	gog_renderer_push_style (view->renderer, sobj->style);
 	gog_renderer_draw_rectangle (view->renderer, &view->allocation, NULL);
 	gog_renderer_pop_style (view->renderer);
-	(oview_parent_klass->render) (view, bbox);
+
+	if (klass->call_parent_render)
+		(oview_parent_klass->render) (view, bbox);
 }
 
 static void
-gog_outlined_view_class_init (GogViewClass *view_klass)
+gog_outlined_view_class_init (GogOutlinedViewClass *oview_klass)
 {
+	GogViewClass *view_klass = (GogViewClass *) oview_klass;
+
 	oview_parent_klass = g_type_class_peek_parent (view_klass);
 	view_klass->size_request  = gog_outlined_view_size_request;
 	view_klass->size_allocate = gog_outlined_view_size_allocate;
 	view_klass->render	  = gog_outlined_view_render;
+
+	oview_klass->call_parent_render = TRUE;
 }
 
 GSF_CLASS (GogOutlinedView, gog_outlined_view,
