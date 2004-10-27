62527b49eedd2850b369c1c9c0876afd395f8d4c
Morten Welinder <terra@gnome.org>
2004-10-27 19:55:24 +0000

    Improve accuracy.
    2004-10-27  Morten Welinder  <terra@gnome.org>
    
    	* src/complex.c (complex_div, complex_sqrt): Improve accuracy.

--- src/complex.c
+++ src/complex.c
@@ -136,3 +136,87 @@ complex_from_string (complex_t *dst, const char *src, char *imunit)
 }
 
 /* ------------------------------------------------------------------------- */
+
+void
+complex_to_polar (gnm_float *mod, gnm_float *angle, const complex_t *src)
+{
+	*mod = complex_mod (src);
+	*angle = complex_angle (src);
+}
+
+/* ------------------------------------------------------------------------- */
+
+void
+complex_from_polar (complex_t *dst, gnm_float mod, gnm_float angle)
+{
+	complex_init (dst, mod * cosgnum (angle), mod * singnum (angle));
+}
+
+/* ------------------------------------------------------------------------- */
+
+void
+complex_mul (complex_t *dst, const complex_t *a, const complex_t *b)
+{
+	complex_init (dst,
+		      a->re * b->re - a->im * b->im,
+		      a->re * b->im + a->im * b->re);
+}
+
+/* ------------------------------------------------------------------------- */
+
+void
+complex_div (complex_t *dst, const complex_t *a, const complex_t *b)
+{
+	gnm_float bmod = complex_mod (b);
+
+	if (bmod >= GNM_const(1e10)) {
+		/* Ok, it's big.  */
+		gnm_float a_re = a->re / bmod;
+		gnm_float a_im = a->im / bmod;
+		gnm_float b_re = b->re / bmod;
+		gnm_float b_im = b->im / bmod;
+		complex_init (dst,
+			      a_re * b_re + a_im * b_im,
+			      a_im * b_re - a_re * b_im);
+	} else {
+		gnm_float bmodsqr = bmod * bmod;
+		complex_init (dst,
+			      (a->re * b->re + a->im * b->im) / bmodsqr,
+			      (a->im * b->re - a->re * b->im) / bmodsqr);
+	}
+}
+
+/* ------------------------------------------------------------------------- */
+
+void
+complex_sqrt (complex_t *dst, const complex_t *src)
+{
+	if (complex_real_p (src)) {
+		if (src->re >= 0)
+			complex_init (dst, sqrtgnum (src->re), 0);
+		else
+			complex_init (dst, 0, sqrtgnum (-src->re));
+	} else
+		complex_from_polar (dst,
+				    sqrtgnum (complex_mod (src)),
+				    complex_angle (src) / 2);
+}
+
+/* ------------------------------------------------------------------------- */
+
+void
+complex_pow (complex_t *dst, const complex_t *a, const complex_t *b)
+{
+	complex_t lna, b_lna;
+
+	/* ln is not defined for reals less than or equal to zero.  */
+	if (complex_real_p (a) && complex_real_p (b))
+		complex_init (dst, powgnum (a->re, b->re), 0);
+	else {
+		complex_ln (&lna, a);
+		complex_mul (&b_lna, b, &lna);
+		complex_exp (dst, &b_lna);
+	}
+}
+
+/* ------------------------------------------------------------------------- */
--- src/complex.h
+++ src/complex.h
@@ -38,6 +38,13 @@ char *complex_to_string (const complex_t *src, const char *reformat,
 
 int complex_from_string (complex_t *dst, const char *src, char *imunit);
 
+void complex_to_polar (gnm_float *mod, gnm_float *angle, const complex_t *src);
+void complex_from_polar (complex_t *dst, gnm_float mod, gnm_float angle);
+void complex_mul (complex_t *dst, const complex_t *a, const complex_t *b);
+void complex_div (complex_t *dst, const complex_t *a, const complex_t *b);
+void complex_sqrt (complex_t *dst, const complex_t *src);
+void complex_pow (complex_t *dst, const complex_t *a, const complex_t *b);
+
 /* ------------------------------------------------------------------------- */
 
 GNUMERIC_COMPLEX_PROTO (void complex_init (complex_t *dst, gnm_float re, gnm_float im))
@@ -96,25 +103,6 @@ GNUMERIC_COMPLEX_PROTO (gnm_float complex_angle (const complex_t *src))
 
 /* ------------------------------------------------------------------------- */
 
-GNUMERIC_COMPLEX_PROTO (void complex_to_polar (gnm_float *mod, gnm_float *angle, const complex_t *src))
-#ifdef GNUMERIC_COMPLEX_BODY
-{
-	*mod = complex_mod (src);
-	*angle = complex_angle (src);
-}
-#endif
-
-/* ------------------------------------------------------------------------- */
-
-GNUMERIC_COMPLEX_PROTO (void complex_from_polar (complex_t *dst, gnm_float mod, gnm_float angle))
-#ifdef GNUMERIC_COMPLEX_BODY
-{
-	complex_init (dst, mod * cosgnum (angle), mod * singnum (angle));
-}
-#endif
-
-/* ------------------------------------------------------------------------- */
-
 GNUMERIC_COMPLEX_PROTO (void complex_conj (complex_t *dst, const complex_t *src))
 #ifdef GNUMERIC_COMPLEX_BODY
 {
@@ -152,31 +140,6 @@ GNUMERIC_COMPLEX_PROTO (void complex_sub (complex_t *dst, const complex_t *a, co
 
 /* ------------------------------------------------------------------------- */
 
-GNUMERIC_COMPLEX_PROTO (void complex_mul (complex_t *dst, const complex_t *a, const complex_t *b))
-#ifdef GNUMERIC_COMPLEX_BODY
-{
-	complex_init (dst,
-		      a->re * b->re - a->im * b->im,
-		      a->re * b->im + a->im * b->re);
-}
-#endif
-
-/* ------------------------------------------------------------------------- */
-
-GNUMERIC_COMPLEX_PROTO (void complex_div (complex_t *dst, const complex_t *a, const complex_t *b))
-#ifdef GNUMERIC_COMPLEX_BODY
-{
-	gnm_float modsqr;
-
-	modsqr = b->re * b->re + b->im * b->im;
-	complex_init (dst,
-		      (a->re * b->re + a->im * b->im) / modsqr,
-		      (a->im * b->re - a->re * b->im) / modsqr);
-}
-#endif
-
-/* ------------------------------------------------------------------------- */
-
 GNUMERIC_COMPLEX_PROTO (void complex_exp (complex_t *dst, const complex_t *src))
 #ifdef GNUMERIC_COMPLEX_BODY
 {
@@ -194,24 +157,6 @@ GNUMERIC_COMPLEX_PROTO (void complex_ln (complex_t *dst, const complex_t *src))
 	complex_init (dst,
 		      loggnum (complex_mod (src)),
 		      complex_angle (src));
-}
-#endif
-
-/* ------------------------------------------------------------------------- */
-
-GNUMERIC_COMPLEX_PROTO (void complex_pow (complex_t *dst, const complex_t *a, const complex_t *b))
-#ifdef GNUMERIC_COMPLEX_BODY
-{
-	complex_t lna, b_lna;
-
-	/* ln is not defined for reals less than or equal to zero.  */
-	if (complex_real_p (a) && complex_real_p (b))
-		complex_init (dst, powgnum (a->re, b->re), 0);
-	else {
-		complex_ln (&lna, a);
-		complex_mul (&b_lna, b, &lna);
-		complex_exp (dst, &b_lna);
-	}
 }
 #endif
 
@@ -247,17 +192,6 @@ GNUMERIC_COMPLEX_PROTO (void complex_tan (complex_t *dst, const complex_t *src))
 	complex_sin (&s, src);
 	complex_cos (&c, src);
 	complex_div (dst, &s, &c);
-}
-#endif
-
-/* ------------------------------------------------------------------------- */
-
-GNUMERIC_COMPLEX_PROTO (void complex_sqrt (complex_t *dst, const complex_t *src))
-#ifdef GNUMERIC_COMPLEX_BODY
-{
-	complex_from_polar (dst,
-			    sqrtgnum (complex_mod (src)),
-			    complex_angle (src) / 2);
 }
 #endif
 
