bf6936357ab7d9cb34eb41311c6a16ec5c92171f
Stepan Kasal <kasal@ucw.cz>
2004-11-01 04:20:55 +0000

    A cleanup of ssconvert; don't require Gnome.
    2004-10-30  Stepan Kasal  <kasal@ucw.cz>
    
    	A cleanup of ssconvert; don't require Gnome.
    
    	* src/ssconvert.c (list_them): New general function which lists
    	  importers or exporters.
    	(convert): New function; the main functionality moved here.
    	(mail): Use the above functions; call gsf_init and don't use
    	  Gnome-specific functions.
    
    2004-09-13  Stepan Kasal  <kasal@ucw.cz>
    
    	* configure.in: Don't use test's operators -a and -o, they aren't
    	  portable.  A small cleanup of the python detection code.

--- src/ssconvert.c
+++ src/ssconvert.c
@@ -13,19 +13,15 @@
 #include <glib/gi18n.h>
 #include "gnumeric.h"
 #include "libgnumeric.h"
-#include <src/gnumeric-paths.h>
-#include <src/plugin.h>
-#include <src/command-context-stderr.h>
-#include <src/io-context.h>
-#include <src/workbook-view.h>
-#include <src/file.h>
+#include "gnumeric-paths.h"
+#include "plugin.h"
+#include "command-context-stderr.h"
+#include "io-context.h"
+#include "workbook-view.h"
+#include "file.h"
 #include <goffice/utils/go-file.h>
+#include <gsf/gsf-utils.h>
 #include <string.h>
-
-#ifdef WITH_GNOME
-#include <libgnome/gnome-program.h>
-#include <libgnome/gnome-init.h>
-#endif
 
 static gboolean ssconvert_show_version = FALSE;
 static gboolean ssconvert_list_exporters = FALSE;
@@ -35,7 +31,9 @@ static char const *ssconvert_import_encoding = NULL;
 static char const *ssconvert_export_id = NULL;
 
 const struct poptOption
-gnumeric_popt_options[] = {
+ssconvert_popt_options[] = {
+	{ NULL, '\0', POPT_ARG_INTL_DOMAIN, (char *)GETTEXT_PACKAGE, 0, NULL, NULL },
+
 	{ "version", 'v', POPT_ARG_NONE, &ssconvert_show_version, 0,
 	  N_("Display Gnumeric's version"), NULL  },
 
@@ -55,35 +53,127 @@ gnumeric_popt_options[] = {
 	{ "export-file-per-sheet", 'S', POPT_ARG_NONE, &ssconvert_one_file_per_sheet, 0,
 	  N_("Export a file for each sheet if the exporter only supports one sheet at a time."), NULL },
 
-	{ NULL, '\0', 0, NULL, 0 }
+	POPT_AUTOHELP
+	POPT_TABLEEND
 };
 
-int
-main (int argc, char *argv [])
+typedef GList *(*get_them_f)(void);
+typedef gchar const *(*get_desc_f)(void *);
+
+static void
+list_them (get_them_f get_them,
+	   get_desc_f get_his_id,
+	   get_desc_f get_his_description)
 {
-	GnomeProgram	*program;
-	GnmCmdContext	*cc;
+	GList *ptr;
+	unsigned tmp, len = 0;
+
+	for (ptr = (*get_them) (); ptr ; ptr = ptr->next) {
+		tmp = strlen ((*get_his_id) (ptr->data));
+		if (len < tmp)
+			len = tmp;
+	}
+
+	fputs ("ID", stderr);
+	for (tmp = 2 ; tmp++ < len ;)
+		fputc (' ', stderr);
+	fputs ("  Description\n", stderr);
+	for (ptr = (*get_them) (); ptr ; ptr = ptr->next) {
+		tmp = strlen ((*get_his_id) (ptr->data));
+		fputs ((*get_his_id) (ptr->data), stderr);
+		while (tmp++ < len)
+			fputc (' ', stderr);
+		fprintf (stderr, " | %s\n",
+			(*get_his_description) (ptr->data));
+	}
+}
+
+static int
+convert (char const **args, GnmCmdContext *cc)
+{
+	int res = 0;
+	GnmFileSaver *fs = NULL;
+	char *outfile = go_shell_arg_to_uri (args[1]);
+
+	if (ssconvert_export_id != NULL) {
+		fs = gnm_file_saver_for_id (ssconvert_export_id);
+		if (fs == NULL) {
+			res = 1;
+			fprintf (stderr, _("Unknown exporter '%s'.\n"
+				 "Try --list-exporters to see a list of possibilities.\n"),
+				 ssconvert_export_id);
+		} else if (outfile == NULL &&
+			   gnm_file_saver_get_extension	(fs) != NULL) {
+			char *basename = g_path_get_basename (args[0]);
+			if (basename != NULL) {
+				char *t = strrchr (basename, '.');
+				if (t != NULL) {
+					GString *res = g_string_new (NULL);
+					t = strrchr (args[0], '.');
+					g_string_append_len (res, args[0], t - args[0] + 1);
+					g_string_append (res, 
+						gnm_file_saver_get_extension (fs));
+					outfile = g_string_free (res, FALSE);
+				}
+				g_free (basename);
+			}
+		}
+	} else {
+		if (outfile != NULL) {
+			fs = gnm_file_saver_for_file_name (outfile);
+			if (fs == NULL) {
+				res = 2;
+				fprintf (stderr, _("Unable to guess exporter to use for '%s'.\n"
+					 "Try --list-exporters to see a list of possibilities.\n"),
+					 outfile);
+			}
+		}
+	}
+	if (outfile == NULL)
+		fprintf (stderr, _("An output file name or an explicit export type is required.\n"
+			 "Try --list-exporters to see a list of possibilities.\n"));
+
+	if (fs != NULL) {
+		IOContext *io_context = gnumeric_io_context_new (cc);
+		char *uri = go_shell_arg_to_uri (args[0]);
+		WorkbookView *wbv = wb_view_new_from_uri (uri, NULL,
+			io_context, ssconvert_import_encoding);
+		g_free (uri);
+		if (gnm_file_saver_get_save_scope (fs) !=
+		    FILE_SAVE_WORKBOOK) {
+			if (ssconvert_one_file_per_sheet) {
+				g_warning ("TODO");
+			} else
+				fprintf (stderr, _("Selected exporter (%s) does not support saving multiple sheets in one file.\n"
+						   "Only the current sheet will be saved."),
+					 gnm_file_saver_get_id (fs));
+		}
+		res = !wb_view_save_as (wbv, fs, outfile, cc);
+		g_object_unref (wb_view_workbook (wbv));
+		g_object_unref (io_context);
+	}
+	return res;
+}
+
+int
+main (int argc, char const *argv [])
+{
 	ErrorInfo	*plugin_errs;
 	int		 res = 0;
+	GnmCmdContext	*cc;
 	poptContext ctx;
 
 	gnm_pre_parse_init (argv[0]);
-	program = gnome_program_init (PACKAGE, VERSION,
-		LIBGNOME_MODULE, argc, argv,
-		GNOME_PARAM_APP_PREFIX,		GNUMERIC_PREFIX,
-		GNOME_PARAM_APP_SYSCONFDIR,	GNUMERIC_SYSCONFDIR,
-		GNOME_PARAM_APP_DATADIR,	gnumeric_data_dir,
-		GNOME_PARAM_APP_LIBDIR,		gnumeric_lib_dir,
-		GNOME_PARAM_POPT_TABLE,		gnumeric_popt_options,
-		NULL);
+	gsf_init();
 
-	g_object_get (G_OBJECT (program),
-		GNOME_PARAM_POPT_CONTEXT,	&ctx,
-		NULL);
+	ctx = poptGetContext (NULL, argc, argv, ssconvert_popt_options, 0);
+	while (poptGetNextOpt (ctx) > 0)
+		;
 
 	if (ssconvert_show_version) {
 		printf (_("ssconvert version '%s'\ndatadir := '%s'\nlibdir := '%s'\n"),
 			GNUMERIC_VERSION, gnumeric_data_dir, gnumeric_lib_dir);
+		poptFreeContext (ctx);
 		return 0;
 	}
 
@@ -91,119 +181,28 @@ main (int argc, char *argv [])
 
 	cc = cmd_context_stderr_new ();
 	plugins_init (cc);
-	if (ctx) {
+	plugin_db_activate_plugin_list (
+		plugins_get_available_plugins (), &plugin_errs);
+
+	if (ssconvert_list_exporters)
+		list_them (&get_file_savers,
+			(get_desc_f) &gnm_file_saver_get_id,
+			(get_desc_f) &gnm_file_saver_get_description);
+	else if (ssconvert_list_importers)
+		list_them (&get_file_openers,
+			(get_desc_f) &gnm_file_opener_get_id,
+			(get_desc_f) &gnm_file_opener_get_description);
+	else {
 		char const **args = poptGetArgs (ctx);
-		plugin_db_activate_plugin_list (
-			plugins_get_available_plugins (), &plugin_errs);
-
-		if (ssconvert_list_exporters) {
-			GList *ptr;
-			unsigned tmp, len = 0;
-
-			for (ptr = get_file_savers (); ptr ; ptr = ptr->next) {
-				tmp = strlen (gnm_file_saver_get_id (ptr->data));
-				if (len < tmp)
-					len = tmp;
-			}
-
-			fputs ("ID", stderr);
-			for (tmp = 2 ; tmp++ < len ;)
-				fputc (' ', stderr);
-			fputs ("  Description\n", stderr);
-			for (ptr = get_file_savers (); ptr ; ptr = ptr->next) {
-				tmp = strlen (gnm_file_saver_get_id (ptr->data));
-				fputs (gnm_file_saver_get_id (ptr->data), stderr);
-				while (tmp++ < len)
-					fputc (' ', stderr);
-				fprintf (stderr, " | %s\n",
-					gnm_file_saver_get_description (ptr->data));
-			}
-		} else if (ssconvert_list_importers) {
-			GList *ptr;
-			unsigned tmp, len = 0;
-
-			for (ptr = get_file_openers (); ptr ; ptr = ptr->next) {
-				tmp = strlen (gnm_file_opener_get_id (ptr->data));
-				if (len < tmp)
-					len = tmp;
-			}
-
-			fputs ("ID", stderr);
-			for (tmp = 2 ; tmp++ < len ;)
-				fputc (' ', stderr);
-			fputs ("  Description\n", stderr);
-			for (ptr = get_file_openers (); ptr ; ptr = ptr->next) {
-				tmp = strlen (gnm_file_opener_get_id (ptr->data));
-				fputs (gnm_file_opener_get_id (ptr->data), stderr);
-				while (tmp++ < len)
-					fputc (' ', stderr);
-				fprintf (stderr, " | %s\n",
-					gnm_file_opener_get_description (ptr->data));
-			}
-		} else if (args && args[0]) {
-			GnmFileSaver *fs = NULL;
-			char *outfile = go_shell_arg_to_uri (args[1]);
-
-			if (ssconvert_export_id != NULL) {
-				fs = gnm_file_saver_for_id (ssconvert_export_id);
-				if (fs == NULL) {
-					res = 1;
-					fprintf (stderr, _("Unknown exporter '%s'.\n"
-						 "Try --list-exporters to see a list of possibilities.\n"),
-						 ssconvert_export_id);
-				} else if (outfile == NULL &&
-					   gnm_file_saver_get_extension	(fs) != NULL) {
-					char *basename = g_path_get_basename (args[0]);
-					if (basename != NULL) {
-						char *t = strrchr (basename, '.');
-						if (t != NULL) {
-							GString *res = g_string_new (NULL);
-							t = strrchr (args[0], '.');
-							g_string_append_len (res, args[0], t - args[0] + 1);
-							g_string_append (res, 
-								gnm_file_saver_get_extension (fs));
-							outfile = g_string_free (res, FALSE);
-						}
-						g_free (basename);
-					}
-				}
-			} else {
-				if (outfile != NULL) {
-					fs = gnm_file_saver_for_file_name (outfile);
-					if (fs == NULL) {
-						res = 2;
-						fprintf (stderr, _("Unable to guess exporter to use for '%s'.\n"
-							 "Try --list-exporters to see a list of possibilities.\n"),
-							 outfile);
-					}
-				}
-			}
-			if (outfile == NULL)
-				fprintf (stderr, _("An output file name or an explicit export type is required.\n"
-					 "Try --list-exporters to see a list of possibilities.\n"));
-
-			if (fs != NULL) {
-				IOContext *io_context = gnumeric_io_context_new (cc);
-				char *uri = go_shell_arg_to_uri (args[0]);
-				WorkbookView *wbv = wb_view_new_from_uri (uri, NULL,
-					io_context, ssconvert_import_encoding);
-				g_free (uri);
-				if (gnm_file_saver_get_save_scope (fs) !=
-				    FILE_SAVE_WORKBOOK) {
-					if (ssconvert_one_file_per_sheet) {
-						g_warning ("TODO");
-					} else
-						fprintf (stderr, _("Selected exporter (%s) does not support saving multiple sheets in one file.\n"
-								   "Only the current sheet will be saved."),
-							 gnm_file_saver_get_id (fs));
-				}
-				res = !wb_view_save_as (wbv, fs, outfile, cc);
-				g_object_unref (wb_view_workbook (wbv));
-				g_object_unref (io_context);
-			}
-		} else
+		if (args && args[0])
+			res = convert (args, cc);
+		else {
 			poptPrintUsage(ctx, stderr, 0);
+			res = 1;
+		}
 	}
+
+	poptFreeContext (ctx);
 	g_object_unref (cc);
 	gnm_shutdown ();
 
