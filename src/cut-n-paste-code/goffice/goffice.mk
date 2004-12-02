9a7532e92f1faa31b15e8021652aff27481c4143
Jody Goldberg <jody@gnome.org>
2004-12-02 13:57:26 +0000

    remove GNUMERIC_ICON_DIR
    2004-12-01  Jody Goldberg <jody@gnome.org>
    
    	* goffice.mk (INCLUDES) : remove GNUMERIC_ICON_DIR

--- src/cut-n-paste-code/goffice/goffice.mk
+++ src/cut-n-paste-code/goffice/goffice.mk
@@ -2,9 +2,9 @@
 INCLUDES = -I$(top_srcdir) 					\
 	   -I$(top_srcdir)/src 					\
 	   -I$(top_builddir)/src 				\
+	   -I$(top_srcdir)/src/cut-n-paste-code			\
 	   -I$(top_srcdir)/src/cut-n-paste-code/goffice		\
 	   -I$(top_srcdir)/src/cut-n-paste-code/foocanvas	\
-	    $(GNUMERIC_CFLAGS) \
-	   -DGNUMERICICONDIR=\"$(gnumeric_icondir)\"
+	    $(GNUMERIC_CFLAGS)
 
 GOFFICE_PLUGIN_FLAGS = $(GNUMERIC_PLUGIN_LDFLAGS)
