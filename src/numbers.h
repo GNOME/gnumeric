6350de99dda67ce056161191cc5210e11acc0fa6
Jody Goldberg <jody@src.gnome.org>
2005-03-05 02:07:14 +0000

    move back from goffice

--- src/numbers.h
+++ src/numbers.h
@@ -1,7 +1,9 @@
 #ifndef GNUMERIC_NUMBERS_H
 #define GNUMERIC_NUMBERS_H
 
-#include <math.h>
+#include <gnumeric-config.h>
+#include <goffice/utils/go-math.h>
+
 #ifdef HAVE_IEEEFP_H
 #include <ieeefp.h>
 #endif
@@ -11,207 +13,164 @@
 
 #ifdef WITH_LONG_DOUBLE
 
-#ifdef HAVE_SUNMATH_H
-#include <sunmath.h>
-#endif
-
 typedef long double gnm_float;
-#ifdef HAVE_STRTOLD
-#ifdef MUST_PROTOTYPE_STRTOLD
-long double strtold (const char *, char **);
-#endif
-#define strtognum strtold
-#else
-#define NEED_FAKE_STRTOGNUM
-/* Defined in gutils.c  */
-gnm_float strtognum (const char *str, char **end);
-#endif
-
-#ifdef HAVE_MODFL
-#define modfgnum modfl
-#else
-#define NEED_FAKE_MODFGNUM
-/* Defined in gutils.c  */
-gnm_float modfgnum (gnm_float x, gnm_float *iptr);
-#endif
-
-#ifdef HAVE_LDEXPL
-#define ldexpgnum ldexpl
-#else
-#define NEED_FAKE_LDEXPGNUM
-/* Defined in gutils.c  */
-gnm_float ldexpgnum (gnm_float x, int exp);
-#endif
-
-#ifdef HAVE_FREXPL
-#define frexpgnum frexpl
-#else
-#define NEED_FAKE_FREXPGNUM
-/* Defined in gutils.c  */
-gnm_float frexpgnum (gnm_float x, int *exp);
-#endif
 
 #ifdef HAVE_ERF
-#define erfgnum erfl
+#define gnm_erf erfl
 #else
 #define NEED_FAKE_ERFGNUM
 /* Defined in gutils.c  */
-gnm_float erfgnum (gnm_float x);
+gnm_float gnm_erf (gnm_float x);
 #endif
 
 #ifdef HAVE_ERFC
-#define erfcgnum erfcl
+#define gnm_erfc erfcl
 #else
 #define NEED_FAKE_ERFCGNUM
 /* Defined in gutils.c  */
-gnm_float erfcgnum (gnm_float x);
+gnm_float gnm_erfc (gnm_float x);
 #endif
 
 #ifdef HAVE_YNL
-#define yngnum ynl
+#define gnm_yn ynl
 #else
 #define NEED_FAKE_YNGNUM
 /* Defined in gutils.c  */
-gnm_float yngnum (int n, gnm_float x);
+gnm_float gnm_yn (int n, gnm_float x);
 #endif
 
-#define acosgnum acosl
-#define acoshgnum acoshl
-#define asingnum asinl
-#define asinhgnum asinhl
-#define atan2gnum atan2l
-#define atangnum atanl
-#define atanhgnum atanhl
-#define ceilgnum ceill
-#define cosgnum cosl
-#define coshgnum coshl
-#define expgnum expl
-#define expm1gnum expm1l
-#define finitegnum finitel
-#define floorgnum floorl
-#define fmodgnum fmodl
-#define gnumabs fabsl
-#define hypotgnum hypotl
-#define isnangnum isnanl
-#define lgammagnum lgammal
-#define lgamma_rgnum lgammal_r
-#define log10gnum log10l
-#define log1pgnum log1pl
-#define loggnum logl
-#define powgnum powl
-#define singnum sinl
-#define sinhgnum sinhl
-#define sqrtgnum sqrtl
-#define tangnum tanl
-#define tanhgnum tanhl
+#define gnm_acos acosl
+#define gnm_acosh acoshl
+#define gnm_asin asinl
+#define gnm_asinh asinhl
+#define gnm_atan2 atan2l
+#define gnm_atan atanl
+#define gnm_atanh atanhl
+#define gnm_ceil ceill
+#define gnm_cos cosl
+#define gnm_cosh coshl
+#define gnm_exp expl
+#define gnm_expm1 expm1l
+#define gnm_finite finitel
+#define gnm_floor floorl
+#define gnm_fmod fmodl
+#define gnm_abs fabsl
+#define gnm_frexp frexpl
+#define gnm_hypot hypotl
+#define gnm_isnan isnanl
+#define gnm_ldexp ldexpl
+#define gnm_lgamma lgammal
+#define gnm_lgamma_r lgammal_r
+#define gnm_log10 log10l
+#define gnm_log1p log1pl
+#define gnm_log logl
+#define gnm_modf modfl
+#define gnm_pow powl
+#define gnm_pow2 go_pow2l
+#define gnm_pow10 go_pow10l
+#define gnm_sin sinl
+#define gnm_sinh sinhl
+#define gnm_sqrt sqrtl
+#define gnm_strto strtold
+#define gnm_tan tanl
+#define gnm_tanh tanhl
 
-#define GNUM_FORMAT_e "Le"
-#define GNUM_FORMAT_E "LE"
-#define GNUM_FORMAT_f "Lf"
-#define GNUM_FORMAT_g "Lg"
-#define GNUM_FORMAT_G "LG"
-#define GNUM_DIG LDBL_DIG
-#define GNUM_MANT_DIG LDBL_MANT_DIG
-#define GNUM_MIN_EXP LDBL_MIN_EXP
-#define GNUM_MAX_EXP LDBL_MAX_EXP
-#define GNUM_MIN LDBL_MIN
-#define GNUM_MAX LDBL_MAX
-#define GNUM_EPSILON LDBL_EPSILON
-#define GNM_const(_c) _c ## L
+#define gnm_add_epsilon go_add_epsilonl
+#define gnm_sub_epsilon go_sub_epsilonl
+#define gnm_fake_floor go_fake_floorl
+#define gnm_fake_ceil go_fake_ceill
+#define gnm_fake_round go_fake_roundl
+#define gnm_fake_trunc go_fake_truncl
+#define gnm_render_number go_render_numberl
+#define gnm_fmt_general_float go_fmt_general_floatl
+#define gnm_format_number go_format_numberl
+
+#define GNM_FORMAT_e	"Le"
+#define GNM_FORMAT_E	"LE"
+#define GNM_FORMAT_f	"Lf"
+#define GNM_FORMAT_g	"Lg"
+#define GNM_FORMAT_G	"LG"
+#define GNM_DIG		LDBL_DIG
+#define GNM_MANT_DIG	LDBL_MANT_DIG
+#define GNM_MIN_EXP	LDBL_MIN_EXP
+#define GNM_MAX_EXP	LDBL_MAX_EXP
+#define GNM_MIN		LDBL_MIN
+#define GNM_MAX		LDBL_MAX
+#define GNM_EPSILON	LDBL_EPSILON
+#define GNM_const(_c)	_c ## L
 
 #else /* !WITH_LONG_DOUBLE */
 
 typedef double gnm_float;
 
-#define acosgnum acos
-#define acoshgnum acosh
-#define asingnum asin
-#define asinhgnum asinh
-#define atan2gnum atan2
-#define atangnum atan
-#define atanhgnum atanh
-#define ceilgnum ceil
-#define cosgnum cos
-#define coshgnum cosh
-#define erfcgnum erfc
-#define erfgnum erf
-#define expgnum exp
-#define expm1gnum expm1
-#define floorgnum floor
-#define fmodgnum fmod
-#define frexpgnum frexp
-#define gnumabs fabs
-#define hypotgnum hypot
-#define isnangnum isnan
-#define ldexpgnum ldexp
-#define lgammagnum lgamma
-#define lgamma_rgnum lgamma_r
-#define log10gnum log10
-#define log1pgnum log1p
-#define loggnum log
-#define modfgnum modf
-#define powgnum pow
-#define singnum sin
-#define sinhgnum sinh
-#define sqrtgnum sqrt
-#define strtognum strtod
-#define tangnum tan
-#define tanhgnum tanh
-#define yngnum yn
+#define gnm_acos acos
+#define gnm_acosh acosh
+#define gnm_asin asin
+#define gnm_asinh asinh
+#define gnm_atan2 atan2
+#define gnm_atan atan
+#define gnm_atanh atanh
+#define gnm_ceil ceil
+#define gnm_cos cos
+#define gnm_cosh cosh
+#define gnm_erfc erfc
+#define gnm_erf erf
+#define gnm_exp exp
+#define gnm_expm1 expm1
+#define gnm_finite go_finite
+#define gnm_floor floor
+#define gnm_fmod fmod
+#define gnm_frexp frexp
+#define gnm_abs fabs
+#define gnm_hypot hypot
+#define gnm_isnan isnan
+#define gnm_ldexp ldexp
+#define gnm_lgamma lgamma
+#define gnm_lgamma_r lgamma_r
+#define gnm_log10 log10
+#define gnm_log1p log1p
+#define gnm_log log
+#define gnm_modf modf
+#define gnm_pow pow
+#define gnm_pow2 go_pow2
+#define gnm_pow10 go_pow10
+#define gnm_sin sin
+#define gnm_sinh sinh
+#define gnm_sqrt sqrt
+#define gnm_strto strtod
+#define gnm_tan tan
+#define gnm_tanh tanh
+#define gnm_yn yn
 
-/* What a circus!  */
-#ifdef HAVE_FINITE
-#define finitegnum finite
-#elif defined(HAVE_ISFINITE)
-#define finitegnum isfinite
-#elif defined(FINITE)
-#define finitegnum FINITE
-#error "I don't know an equivalent of finite for your system; you lose"
-#endif
+#define gnm_add_epsilon go_add_epsilon
+#define gnm_sub_epsilon go_sub_epsilon
+#define gnm_fake_floor go_fake_floor
+#define gnm_fake_ceil go_fake_ceil
+#define gnm_fake_round go_fake_round
+#define gnm_fake_trunc go_fake_trunc
+#define gnm_render_number go_render_number
+#define gnm_fmt_general_float go_fmt_general_float
+#define gnm_format_number go_format_number
 
 #ifndef HAVE_LGAMMA_R
 #define NEED_FAKE_LGAMMA_R
 /* Defined in gutils.c  */
-gnm_float lgamma_rgnum (gnm_float x, int *signp);
+gnm_float gnm_lgamma_r (gnm_float x, int *signp);
 #endif
 
-#ifndef HAVE_EXPM1
-#define NEED_FAKE_EXPM1
-/* Defined in gutils.c  */
-gnm_float expm1 (gnm_float x);
-#endif
-
-#ifndef HAVE_ASINH
-#define NEED_FAKE_ASINH
-/* Defined in gutils.c  */
-gnm_float asinh (gnm_float x);
-#endif
-
-#ifndef HAVE_ACOSH
-#define NEED_FAKE_ACOSH
-/* Defined in gutils.c  */
-gnm_float acosh (gnm_float x);
-#endif
-
-#ifndef HAVE_ATANH
-#define NEED_FAKE_ATANH
-/* Defined in gutils.c  */
-gnm_float atanh (gnm_float x);
-#endif
-
-#define GNUM_FORMAT_e "e"
-#define GNUM_FORMAT_E "E"
-#define GNUM_FORMAT_f "f"
-#define GNUM_FORMAT_g "g"
-#define GNUM_FORMAT_G "G"
-#define GNUM_DIG DBL_DIG
-#define GNUM_MANT_DIG DBL_MANT_DIG
-#define GNUM_MIN_EXP DBL_MIN_EXP
-#define GNUM_MAX_EXP DBL_MAX_EXP
-#define GNUM_MIN DBL_MIN
-#define GNUM_MAX DBL_MAX
-#define GNUM_EPSILON DBL_EPSILON
-#define GNM_const(_c) _c
+#define GNM_FORMAT_e	"e"
+#define GNM_FORMAT_E	"E"
+#define GNM_FORMAT_f	"f"
+#define GNM_FORMAT_g	"g"
+#define GNM_FORMAT_G	"G"
+#define GNM_DIG		DBL_DIG
+#define GNM_MANT_DIG	DBL_MANT_DIG
+#define GNM_MIN_EXP	DBL_MIN_EXP
+#define GNM_MAX_EXP	DBL_MAX_EXP
+#define GNM_MIN		DBL_MIN
+#define GNM_MAX		DBL_MAX
+#define GNM_EPSILON	DBL_EPSILON
+#define GNM_const(_c)	_c
 
 #endif
 
