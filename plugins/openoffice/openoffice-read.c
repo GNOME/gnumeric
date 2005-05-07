f2c1cecfbb84fd3ba9ff64dc0d8eb10a4a574c74
Jean Brefort <jean.brefort@normalesup.org>
2005-05-07 07:24:31 +0000

    use go_setlocale instead of gnm_setlocale. ditto.
    2005-05-07  Jean Brefort  <jean.brefort@normalesup.org>
    
    	* openoffice-read.c: (openoffice_file_open): use go_setlocale
    	instead of gnm_setlocale.
    	* openoffice-write.c: (openoffice_file_save): ditto.

--- plugins/openoffice/openoffice-read.c
+++ plugins/openoffice/openoffice-read.c
@@ -1448,11 +1448,11 @@ openoffice_file_open (GOFileOpener const *fo, IOContext *io_context,
 		return;
 	}
 
-	old_num_locale = g_strdup (gnm_setlocale (LC_NUMERIC, NULL));
-	gnm_setlocale (LC_NUMERIC, "C");
-	old_monetary_locale = g_strdup (gnm_setlocale (LC_MONETARY, NULL));
-	gnm_setlocale (LC_MONETARY, "C");
-	gnm_set_untranslated_bools ();
+	old_num_locale = g_strdup (go_setlocale (LC_NUMERIC, NULL));
+	go_setlocale (LC_NUMERIC, "C");
+	old_monetary_locale = g_strdup (go_setlocale (LC_MONETARY, NULL));
+	go_setlocale (LC_MONETARY, "C");
+	go_set_untranslated_bools ();
 
 	/* init */
 	state.context = io_context;
@@ -1514,10 +1514,10 @@ openoffice_file_open (GOFileOpener const *fo, IOContext *io_context,
 
 	gnm_expr_conventions_free (state.exprconv);
 
-	/* gnm_setlocale restores bools to locale translation */
-	gnm_setlocale (LC_MONETARY, old_monetary_locale);
+	/* go_setlocale restores bools to locale translation */
+	go_setlocale (LC_MONETARY, old_monetary_locale);
 	g_free (old_monetary_locale);
-	gnm_setlocale (LC_NUMERIC, old_num_locale);
+	go_setlocale (LC_NUMERIC, old_num_locale);
 	g_free (old_num_locale);
 }
 
--- plugins/openoffice/openoffice-write.c
+++ plugins/openoffice/openoffice-write.c
@@ -293,11 +293,11 @@ openoffice_file_save (GOFileSaver const *fs, IOContext *ioc,
 	GError *err;
 	unsigned i;
 
-	old_num_locale = g_strdup (gnm_setlocale (LC_NUMERIC, NULL));
-	gnm_setlocale (LC_NUMERIC, "C");
-	old_monetary_locale = g_strdup (gnm_setlocale (LC_MONETARY, NULL));
-	gnm_setlocale (LC_MONETARY, "C");
-	gnm_set_untranslated_bools ();
+	old_num_locale = g_strdup (go_setlocale (LC_NUMERIC, NULL));
+	go_setlocale (LC_NUMERIC, "C");
+	old_monetary_locale = g_strdup (go_setlocale (LC_MONETARY, NULL));
+	go_setlocale (LC_MONETARY, "C");
+	go_set_untranslated_bools ();
 
 	outfile = gsf_outfile_zip_new (output, &err);
 
@@ -314,9 +314,9 @@ openoffice_file_save (GOFileSaver const *fs, IOContext *ioc,
 	gsf_output_close (GSF_OUTPUT (outfile));
 	g_object_unref (G_OBJECT (outfile));
 
-	/* gnm_setlocale restores bools to locale translation */
-	gnm_setlocale (LC_MONETARY, old_monetary_locale);
+	/* go_setlocale restores bools to locale translation */
+	go_setlocale (LC_MONETARY, old_monetary_locale);
 	g_free (old_monetary_locale);
-	gnm_setlocale (LC_NUMERIC, old_num_locale);
+	go_setlocale (LC_NUMERIC, old_num_locale);
 	g_free (old_num_locale);
 }
