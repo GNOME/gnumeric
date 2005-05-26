f33ed35b21d92861e9a6fffdf79e7cd25e7f18ef
Morten Welinder <terra@gnome.org>
2005-05-26 21:13:18 +0000

    New function. (qpois): New implementation based on discpfuncinverter.
    2005-05-26  Morten Welinder  <terra@gnome.org>
    
    	* src/mathfunc.c (discpfuncinverter): New function.
    	(qpois): New implementation based on discpfuncinverter.
    
    2005-05-26  Morten Welinder  <terra@gnome.org>
    
    	* import-R: Do not import qpois.

--- src/mathfunc.c
+++ src/mathfunc.c
@@ -833,106 +833,6 @@ gnm_float ppois(gnm_float x, gnm_float lambda, gboolean lower_tail, gboolean log
 }
 
 /* ------------------------------------------------------------------------ */
-/* Imported src/nmath/qpois.c from R.  */
-/*
- *  Mathlib : A C Library of Special Functions
- *  Copyright (C) 1998 Ross Ihaka
- *  Copyright (C) 2000 The R Development Core Team
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA.
- *
- *  DESCRIPTION
- *
- *	The quantile function of the Poisson distribution.
- *
- *  METHOD
- *
- *	Uses the Cornish-Fisher Expansion to include a skewness
- *	correction to a normal approximation.  This gives an
- *	initial value which never seems to be off by more than
- *	1 or 2.	 A search is then conducted of values close to
- *	this initial start point.
- */
-
-
-gnm_float qpois(gnm_float p, gnm_float lambda, gboolean lower_tail, gboolean log_p)
-{
-    gnm_float mu, sigma, gamma, z, y;
-#ifdef IEEE_754
-    if (gnm_isnan(p) || gnm_isnan(lambda))
-	return p + lambda;
-#endif
-    if(!gnm_finite(lambda))
-	ML_ERR_return_NAN;
-    R_Q_P01_check(p);
-    if(lambda < 0) ML_ERR_return_NAN;
-
-    if (p == R_DT_0) return 0;
-    if (p == R_DT_1) return gnm_pinf;
-
-    if(lambda == 0) return 0;
-
-    mu = lambda;
-    sigma = gnm_sqrt(lambda);
-    gamma = sigma;
-
-    /* Note : "same" code in qpois.c, qbinom.c, qnbinom.c --
-     * FIXME: This is far from optimal [cancellation for p ~= 1, etc]: */
-    if(!lower_tail || log_p) {
-	p = R_DT_qIv(p); /* need check again (cancellation!): */
-	if (p == 0.) return 0;
-	if (p == 1.) return gnm_pinf;
-    }
-    /* temporary hack --- FIXME --- */
-    if (p + 1.01*GNM_EPSILON >= 1.) return gnm_pinf;
-
-    /* y := approx.value (Cornish-Fisher expansion) :  */
-    z = qnorm(p, 0., 1., /*lower_tail*/TRUE, /*log_p*/FALSE);
-    y = gnm_floor(mu + sigma * (z + gamma * (z*z - 1) / 6) + 0.5);
-
-    z = ppois(y, lambda, /*lower_tail*/TRUE, /*log_p*/FALSE);
-
-    /* fuzz to ensure left continuity; 1 - 1e-7 may lose too much : */
-    p *= 1 - 64*GNM_EPSILON;
-
-/*-- Fixme, here y can be way off --
-  should use interval search instead of primitive stepping down or up */
-
-#ifdef maybe_future
-    if((lower_tail && z >= p) || (!lower_tail && z <= p)) {
-#else
-    if(z >= p) {
-#endif
-			/* search to the left */
-	for(;;) {
-	    if(y == 0 ||
-	       (z = ppois(y - 1, lambda, /*l._t.*/TRUE, /*log_p*/FALSE)) < p)
-		return y;
-	    y = y - 1;
-	}
-    }
-    else {		/* search to the right */
-	for(;;) {
-	    y = y + 1;
-	    if((z = ppois(y, lambda, /*l._t.*/TRUE, /*log_p*/FALSE)) >= p)
-		return y;
-	}
-    }
-}
-
-/* ------------------------------------------------------------------------ */
 /* Imported src/nmath/stirlerr.c from R.  */
 /*
  *  AUTHOR
@@ -6074,6 +5974,103 @@ pfuncinverter (gnm_float p, const gnm_float shape[],
 	printf ("--> %.15g\n\n", x);
 #endif
 	return x;
+}
+
+/*
+ * Discrete pfuncs only.  (Specifically: only integer x are allowed.
+ */
+static gnm_float
+discpfuncinverter (gnm_float p, const gnm_float shape[],
+		   gboolean lower_tail, gboolean log_p,
+		   gnm_float xlow, gnm_float xhigh, gnm_float x0,
+		   PFunc pfunc)
+{
+	gboolean have_xlow = gnm_finite (xlow);
+	gboolean have_xhigh = gnm_finite (xhigh);
+	gnm_float step;
+	int i;
+
+	if (p == R_DT_0) return xlow;
+	if (p == R_DT_1) return xhigh;
+
+	if (gnm_finite (x0) && x0 >= xlow && x0 <= xhigh)
+		; /* Nothing -- guess is good.  */
+	else if (have_xlow && have_xhigh)
+		x0 = (xlow + xhigh) / 2;
+	else if (have_xhigh)
+		x0 = xhigh;
+	else if (have_xlow)
+		x0 = xlow;
+	else
+		x0 = 0;
+	x0 = gnm_floor (x0);
+	step = 1 + gnm_floor (gnm_abs (x0) * GNM_EPSILON);
+
+	for (i = 1; 1; i++) {
+		gnm_float ex0 = pfunc (x0, shape, lower_tail, log_p) - p;
+#if 0
+		g_print ("x=%.20g  e=%.20g\n", x0, ex0);
+#endif
+		if (!lower_tail) ex0 = -ex0;
+		if (ex0 <= 0)
+			xlow = x0, have_xlow = TRUE;
+		if (ex0 >= 0)
+			xhigh = x0, have_xhigh = TRUE, step = -gnm_abs (step);
+
+		if (i > 1 && have_xlow && have_xhigh) {
+			gnm_float xmid = gnm_floor ((xlow + xhigh) / 2);
+			if (xmid - xlow < 0.5 ||
+			    xmid - xlow < gnm_abs (xlow) * GNM_EPSILON)
+				return xhigh;
+			x0 = xmid;
+		} else {
+			gnm_float x1 = x0 + step;
+
+			if (x1 >= xlow && x1 <= xhigh) {
+				x0 = x1;
+				step *= 2 * i;
+			} else {
+				/* We went off the edge by walking too fast.  */
+				gnm_float newstep = 1 + gnm_floor (gnm_abs (x0) * GNM_EPSILON);
+				step = (step > 0) ? newstep : -newstep;
+				x1 = x0 + step;
+				if (x1 >= xlow && x1 <= xhigh)
+					continue;
+				/*
+				 * We don't seem to find a finite x on the
+				 * other side of the root.
+				 */			   
+				return (step > 0) ? xhigh : xlow;
+			}
+		}
+	}
+
+	g_assert_not_reached ();
+}
+
+/* ------------------------------------------------------------------------ */
+
+static gnm_float
+ppois1 (gnm_float x, const gnm_float *plambda,
+	gboolean lower_tail, gboolean log_p)
+{
+	return ppois (x, *plambda, lower_tail, log_p);
+}
+
+gnm_float
+qpois (gnm_float p, gnm_float lambda, gboolean lower_tail, gboolean log_p)
+{
+	gnm_float mu = lambda;
+	gnm_float sigma = gnm_sqrt (lambda);
+	gnm_float gamma = 1 / sigma;
+
+	/* Cornish-Fisher expansion:  */
+	gnm_float z = qnorm (p, 0., 1., lower_tail, log_p);
+	gnm_float y = gnm_floor (mu + sigma * (z + gamma * (z * z - 1) / 6) + 0.5);
+
+	return discpfuncinverter (p, &lambda, lower_tail, log_p,
+				  0, gnm_pinf, y,
+				  ppois1);
 }
 
 /* ------------------------------------------------------------------------ */
