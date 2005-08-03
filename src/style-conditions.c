8ee58a1fbfbd8560a90208c665704010f2ee5ac8
Jody Goldberg <jody@gnome.org>
2005-08-03 20:01:34 +0000

    suppress a warning and some debug spew.
    2005-08-03  Jody Goldberg <jody@gnome.org>
    
    	* src/style-conditions.c (gnm_style_conditions_eval) : suppress a
    	  warning and some debug spew.

--- src/style-conditions.c
+++ src/style-conditions.c
@@ -177,7 +177,7 @@ int
 gnm_style_conditions_eval (GnmStyleConditions const *sc, GnmEvalPos const *ep)
 {
 	unsigned i;
-	gboolean use_this;
+	gboolean use_this = FALSE;
 	GnmValue *val;
 	GArray const *conds;
 	GnmStyleCond const *cond;
@@ -206,10 +206,11 @@ gnm_style_conditions_eval (GnmStyleConditions const *sc, GnmEvalPos const *ep)
 
 		case GNM_STYLE_COND_CUSTOM:
 			use_this = value_get_as_bool (val, NULL);
-
+#if 0
 			str = gnm_expr_as_string (cond->expr[0], &pp, gnm_expr_conventions_default);
 			g_print ("'%s' = %s\n", str, use_this ? "true" : "false");
 			g_free (str);
+#endif
 		}
 		value_release (val);
 		if (use_this)
