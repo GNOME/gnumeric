f0a387290869ac2ef6baa53b94eb99b5aa51709f
Morten Welinder <terra@gnome.org>
2005-09-02 02:14:48 +0000

    Make the tooltips use "www x hhh" format.
    2005-09-01  Morten Welinder <terra@gnome.org>
    
    	* src/gnumeric-pane.c (gnm_pane_display_obj_size_tip): Make the
    	tooltips use "www x hhh" format.

--- src/gnumeric-pane.c
+++ src/gnumeric-pane.c
@@ -142,10 +142,10 @@ gnm_pane_display_obj_size_tip (GnmPane *pane, SheetObject const *so)
 	scg_object_coords_to_anchor (scg, coords, &anchor);
 	sheet_object_anchor_to_pts (&anchor, sc_sheet (SHEET_CONTROL (scg)), pts);
 	msg = g_strdup_printf (_("%.1f x %.1f pts\n%d x %d pixels"),
-		MAX (fabs (pts[3] - pts[1]) + 1., 0),
 		MAX (fabs (pts[2] - pts[0]) + 1., 0),
-		MAX ((int)floor (fabs (coords [3] - coords [1]) + 1.5), 0),
-		MAX ((int)floor (fabs (coords [2] - coords [0]) + 1.5), 0));
+		MAX (fabs (pts[3] - pts[1]) + 1., 0),
+		MAX ((int)floor (fabs (coords [2] - coords [0]) + 1.5), 0),
+		MAX ((int)floor (fabs (coords [3] - coords [1]) + 1.5), 0));
 	gtk_label_set_text (GTK_LABEL (pane->size_tip), msg);
 	g_free (msg);
 }
