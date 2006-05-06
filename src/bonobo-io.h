69a028e73357eeba9208a365b3209670e25fa4ca
Jody Goldberg <jody@gnome.org>
2006-05-06 03:42:15 +0000

    start installing the headers as 'libspreadsheet-1-7/ssheet'
    2006-04-30  Jody Goldberg <jody@gnome.org>
    
    	* start installing the headers as 'libspreadsheet-1-7/ssheet'
    
    2006-04-30  Jody Goldberg <jody@gnome.org>
    
    	* src/Makefile.am : remove unused code associated with bonobo.  People
    	  can pull it from cvs archives if desired.
    		: bonobo-io.c
    		: bonobo-io.h
    		: sheet-object-bonobo.c
    		: sheet-object-bonobo.h
    		: sheet-object-container.c
    		: sheet-object-container.h
    		: sheet-object-item.c
    		: sheet-object-item.h

--- src/bonobo-io.h
+++
@@ -1,8 +0,0 @@
-#ifndef _GNM_BONOBO_IO_H
-#define _GNM_BONOBO_IO_H
-
-#include <bonobo/bonobo-object.h>
-
-BonoboObject *gnm_persist_stream_new (WorkbookControl *wbc,
-				      const char* const iid);
-#endif /* _GNM_BONOBO_IO_H */
