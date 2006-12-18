1f0271f14f63e9e38d2f1b73f1d06234b85db855
Jody Goldberg <jody@src.gnome.org>
2006-12-18 00:43:55 +0000

    missed commit

--- src/gnumeric-pane.h
+++ src/gnumeric-pane.h
@@ -77,6 +77,6 @@ void gnm_pane_object_start_resize (GnmPane *pane, GdkEventButton *event,
 
 /* A convenience api */
 SheetObjectView *gnm_pane_object_register (SheetObject *so, FooCanvasItem *view, gboolean selectable);
-SheetObjectView *gnm_pane_widget_register (SheetObject *so, GtkWidget *w, FooCanvasItem *view);
+void		 gnm_pane_widget_register (SheetObject *so, GtkWidget *w, FooCanvasItem *view);
 
 #endif /* GNUMERIC_PANE_H */
