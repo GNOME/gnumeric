82e776ad8a743389ea7eaf7058b5191d16d71e60
Morten Welinder <mortenw@src.gnome.org>
2008-03-07 02:33:27 +0000

    Fix test after sylk fixes.
    svn path=/branches/gnumeric-1-8/; revision=16447

--- test/t5903-sylk.pl
+++ test/t5903-sylk.pl
@@ -9,5 +9,5 @@ use GnumericTest;
 my $mode = ((shift @ARGV) || "check");
 
 &message ("Check the sylk importer.");
-&test_importer ("$samples/sylk/test.sylk", "72277f372f223e88bb743f0345004b057497d6d2", $mode);
-&test_importer ("$samples/sylk/encoding.sylk", "fbe5e755c071e6a2b22b4a37bca7f7c678b039a7", $mode);
+&test_importer ("$samples/sylk/test.sylk", "90f60d33651e057b617a5e21fd5efab3ad1704f0", $mode);
+&test_importer ("$samples/sylk/encoding.sylk", "457029e2e46c3f792111b3e6a02aa007e06c3a67", $mode);
