5345694c6ede90a96985db85fc9b71a689448667
Yukihiro Nakai <nakai@gnome.gr.jp>
2008-09-28 21:44:56 +0000

    Enable again
    2008-09-26  Yukihiro Nakai <nakai@gnome.gr.jp>
    
    	* perl-loader.c: Enable again
    
    
    svn path=/trunk/; revision=16835

--- plugins/perl-func/perl_func.pl
+++ plugins/perl-func/perl_func.pl
@@ -17,3 +17,44 @@ EOS
 sub desc_perl_adder {
   return ("ff", "a,b,c");
 }
+
+sub func_perl_date {
+  my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime;
+  return sprintf("%04d%02d%02d", $year + 1900, ++$mon, $mday);
+}
+
+sub help_perl_date {
+  return<<'EOS';
+@FUNCTION=PERL_DATE
+@SYNTAX=PERL_DATE()
+@DESCRIPTION=
+Return today's date as string.
+EOS
+}
+
+sub desc_perl_date {
+  return ("", "");
+}
+
+sub func_perl_sed {
+  my $arg = shift;
+  my $match = shift;
+  my $newch = shift;
+
+  $arg =~ s/$match/$newch/g;
+
+  return $arg;
+}
+
+sub help_perl_sed {
+  return<<'EOS';
+@FUNCTION=PERL_SED
+@SYNTAX=PERL_SED(a,b,c)
+@DESCRIPTION=
+Substite string with matching pattern. Same as $a =~ s/$b/$c/g .
+EOS
+}
+
+sub desc_perl_sed {
+  return ("sss", "a,b,c");
+}
