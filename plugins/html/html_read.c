e3d8e8a831b4c530440ba639556afa751e8f9018
Jon Kåre Hellan <jhellan@src.gnome.org>
2009-02-06 22:32:20 +0000

    Detect unmarked UTF16LE more robustly.
    	* html_read.c (html_file_open): Detect unmarked UTF16LE more
    	robustly.
    
    svn path=/trunk/; revision=17125

--- plugins/html/html_read.c
+++ plugins/html/html_read.c
@@ -510,9 +510,11 @@ html_file_open (GOFileOpener const *fo, IOContext *io_context,
 			case XML_CHAR_ENCODING_NONE:
 				bomlen = 0;
 				/* Try to detect unmarked UTF16LE
-				   (Firefox drag data) */
-				if (buf[0] >= 0x20 && buf[1] == 0 &&
-				    buf[2] >= 0x20 && buf[3] == 0)
+				   (Firefox Windows clipboard, drag data all platforms) */
+				if ((buf[0] >= 0x20 || g_ascii_isspace(buf[0])) && 
+				    buf[1] == 0 &&
+				    (buf[2] >= 0x20 || g_ascii_isspace(buf[2])) &&
+				    buf[3] == 0)
 					enc =  XML_CHAR_ENCODING_UTF16LE;
 				break;
 			default:
