4ab3e65de1784d7492ac331c009a8b6ae054a45e
Morten Welinder <terra@gnome.org>
2009-05-01 19:26:45 -0400

    Tests: test R1C1 parsing too (by way of sylk file).

--- test/t5903-sylk.pl
+++ test/t5903-sylk.pl
@@ -11,3 +11,4 @@ my $mode = ((shift @ARGV) || "check");
 &message ("Check the sylk importer.");
 &test_importer ("$samples/sylk/test.sylk", "e908820fa86b3e1021856e549f11ebb46786ecab", $mode);
 &test_importer ("$samples/sylk/encoding.sylk", "be9944ed11138b3b0433fe450cfc1ca2bfa5f8a1", $mode);
+&test_importer ("$samples/sylk/app_b2.sylk", "9f97f773c008aa8ea62b6c5b308ac41a7f5194d7", $mode);
