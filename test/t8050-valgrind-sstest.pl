1ca498d167b928d7bf73115db3b813281a49a7cb
Morten Welinder <terra@gnome.org>
2009-08-15 17:23:30 -0400

    Tests: skip t8050 on buggy pango.

--- test/t8050-valgrind-sstest.pl
+++ test/t8050-valgrind-sstest.pl
@@ -7,4 +7,9 @@ use GnumericTest;
 
 &message ("Check sstest with valgrind.");
 
+my $pango = `pkg-config --modversion pango 2>/dev/null`;
+chomp $pango;
+&GnumericTest::report_skip ("Pango version $pango is buggy")
+    if $pango eq '1.24.2';
+
 &test_valgrind ("$sstest all >/dev/null 2>&1", 1);
