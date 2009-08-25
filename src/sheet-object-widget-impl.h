4d60e8c29f563864ecf05372fdf26030ff4dc3bb
Jean Brefort <jean.brefort@normalesup.org>
2009-08-25 20:06:57 +0200

    add missing file

---
+++ src/sheet-object-widget-impl.h
@@ -0,0 +1,15 @@
+/* vim: set sw=8: -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+#ifndef _GNM_SHEET_OBJECT_WIDGET_IMPL_H_
+# define _GNM_SHEET_OBJECT_WIDGET_IMPL_H_
+
+#include "sheet-object-widget.h"
+#include "sheet-object-impl.h"
+
+G_BEGIN_DECLS
+
+typedef SheetObjectView			SOWidgetView;
+typedef SheetObjectViewClass	SOWidgetViewClass;
+
+G_END_DECLS
+
+#endif /* _GNM_SHEET_OBJECT_WIDGET_H_ */
