3f5ceef5d3a5075586498078d25f776f811670d8
Morten Welinder <terra@gnome.org>
2009-11-08 12:24:54 -0500

    tests: run a few tests with G_SLICE=debug-blocks

---
+++ test/t8201-gslice2.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -w
+# -----------------------------------------------------------------------------
+
+use strict;
+use lib ($0 =~ m|^(.*/)| ? $1 : ".");
+use GnumericTest;
+
+&message ("Check core glib slice checker");
+$ENV{'G_SLICE'} = 'debug-blocks';
+
+my $src = "$samples/excel/statfuns.xls";
+&GnumericTest::report_skip ("file $src does not exist") unless -r $src;
+
+my $tmp = "statfuns2.xls";
+&GnumericTest::junkfile ($tmp);
+
+&test_command ("$ssconvert $src $tmp", sub { 1 } );
