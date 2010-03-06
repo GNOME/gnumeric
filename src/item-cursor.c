6a5380091a0c8fa301f35fa8ea24be951326ef6c
Jean Brefort <jean.brefort@normalesup.org>
2010-03-06 08:46:52 +0100

    Fix cursor appearance. [#611076]

--- src/item-cursor.c
+++ src/item-cursor.c
@@ -386,7 +386,7 @@ item_cursor_draw (GocItem const *item, cairo_t *cr)
 		gdk_draw_lines (drawable, ic->gc, points, 5);
 	}
 
-	if (draw_external && draw_internal) {
+	if (draw_external && draw_internal && draw_handle) {
 		if (draw_handle < 2) {
 			points [0].x -= 2;
 			points [1].x -= 2;
