28766bb349151033809df17a1e56f0b5c73c05b5
Morten Welinder <terra@gnome.org>
2011-10-15 12:31:25 -0400

    compilation: fix type issue.

--- src/dialogs/dialog-cell-format.c
+++ src/dialogs/dialog-cell-format.c
@@ -1263,7 +1263,7 @@ draw_border_preview (FormatState *state)
 		gtk_widget_show (GTK_WIDGET (state->border.canvas));
 		gtk_widget_set_size_request (GTK_WIDGET (state->border.canvas),
 					     150, 100);
-		gtk_grid_attach (GTK_CONTAINER (go_gtk_builder_get_widget (state->gui, "border-sample-container")),
+		gtk_grid_attach (GTK_GRID (go_gtk_builder_get_widget (state->gui, "border-sample-container")),
 				   GTK_WIDGET (state->border.canvas), 1, 1, 3, 3);
 		group = GOC_GROUP (goc_canvas_get_root (state->border.canvas));
 
