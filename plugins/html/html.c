90317c6427c75c65857175e4a37bb0a4d1f13fcf
Andreas J Guelzow <aguelzow@pyrshep.ca>
2011-10-27 21:07:02 -0600

    write sub- and superscript attributes to html files
    2011-10-27  Andreas J. Guelzow <aguelzow@pyrshep.ca>
    
    	* html.c (cb_html_attrs_as_string): write sub- and superscript
    	attributes

--- plugins/html/html.c
+++ plugins/html/html.c
@@ -211,6 +211,19 @@ cb_html_attrs_as_string (GsfOutput *output, PangoAttribute *a, html_version_t ve
 /* 			((c->blue & 0xff00) >> 8)); */
 		break;/* ignored */
 	default :
+		if (a->klass->type == 
+		    go_pango_attr_subscript_get_type ()) {
+			if (((GOPangoAttrSubscript *)a)->val) {
+				gsf_output_puts (output, "<sub>");
+				closure = "</sub>";
+			}
+		} else if (a->klass->type == 
+			   go_pango_attr_superscript_get_type ()) {
+			if (((GOPangoAttrSuperscript *)a)->val) {
+				gsf_output_puts (output, "<sup>");
+				closure = "</sup>";
+			}
+		}
 		break; /* ignored */
 	}
 
