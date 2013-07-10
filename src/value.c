7ea71880b4155ac5ad270bba590dcc8f15f8611d
Morten Welinder <terra@gnome.org>
2013-07-10 11:44:53 -0400

    COUNTIF: Fix pattern matching.
    1. Should anchor at end.
    2. Should be case insensitive.

--- src/value.c
+++ src/value.c
@@ -1745,7 +1745,7 @@ parse_criteria (GnmValue const *crit_val, GODateConventions const *date_conv)
 		len = 1;
 	} else {
 		res->fun = criteria_test_match;
-		res->has_rx = (gnm_regcomp_XL (&res->rx, criteria, 0, TRUE, FALSE) == GO_REG_OK);
+		res->has_rx = (gnm_regcomp_XL (&res->rx, criteria, GO_REG_ICASE, TRUE, TRUE) == GO_REG_OK);
 		len = 0;
 	}
 
