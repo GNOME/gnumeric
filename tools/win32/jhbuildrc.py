3c1224631a0bd090ca53f637f1d7da8cf23a9e8f
Morten Welinder <terra@gnome.org>
2013-08-11 14:45:35 -0400

    win32: gtk+ to 3.9.10

--- tools/win32/jhbuildrc.py
+++ tools/win32/jhbuildrc.py
@@ -156,7 +156,7 @@ autogenargs += """ --disable-scrollkeeper --disable-gtk-doc"""
 
 module_autogenargs['atk']	= autogenargs + """ --disable-glibtest --enable-introspection=no"""
 module_autogenargs['gdk-pixbuf'] = autogenargs + """ --disable-introspection  --without-libtiff"""
-module_autogenargs['gtk+']	= autogenargs + """ --disable-glibtest --enable-gdiplus --without-libjasper  --without-libtiff --without-libjpeg --enable-cups=no --enable-introspection=no"""
+module_autogenargs['gtk+']	= autogenargs + """ --disable-glibtest --enable-gdiplus --without-libjasper  --without-libtiff --without-libjpeg --enable-cups=no --enable-introspection=no --with-included-immodules=yes"""
 
 module_autogenargs['libgda']	= autogenargs + """ --without-odbc --without-java --without-libsoup"""
 module_autogenargs['pxlib']	= autogenargs + """ --with-gsf=""" + prefix
