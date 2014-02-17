7b5f5c3c6d38b05de5e699d5314c0b5f545e351b
Morten Welinder <terra@gnome.org>
2014-02-17 13:09:30 -0500

    Tests: test number roundtrips.

---
+++ test/t6501-numbers.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl -w
+# -----------------------------------------------------------------------------
+
+use strict;
+use lib ($0 =~ m|^(.*/)| ? $1 : ".");
+use GnumericTest;
+
+my $file = "$samples/number-tests.gnumeric";
+
+&message ("Check string ods roundtrip.");
+&test_roundtrip ($file, 'Gnumeric_OpenCalc:odf', "ods");
+
+&message ("Check string xls/BIFF7 roundtrip.");
+&test_roundtrip ($file, 'Gnumeric_Excel:excel_biff7', "xls");
+
+&message ("Check string xls/BIFF8 roundtrip.");
+&test_roundtrip ($file, 'Gnumeric_Excel:excel_biff8', "xls");
+
+&message ("Check string xlsx roundtrip.");
+&test_roundtrip ($file, 'Gnumeric_Excel:xlsx', "xlsx");
