0f7a91bf50298db2858ffc13ec7d318ac347c9c3
Morten Welinder <terra@gnome.org>
2014-02-19 18:45:15 -0500

    Tests: drop complains over new codepage attribute.

--- test/t6501-numbers.pl
+++ test/t6501-numbers.pl
@@ -21,16 +21,20 @@ my $file = "$samples/number-tests.gnumeric";
 		 'ext' => "ods",
 		 'filter2' => "perl -p -e '\$_ = \"\" if m{<meta:generator>}'");
 
+my $xls_filter = "perl -p -e '\$_ = \"\" if m{<meta:user-defined meta:name=.msole:codepage.}'";
+
 &message ("Check number xls/BIFF7 roundtrip.");
 &test_roundtrip ($file,
 		 'format' => 'Gnumeric_Excel:excel_biff7',
 		 'ext' => "xls",
-		 'resize' => '16384x256');
+		 'resize' => '16384x256',
+		 'filter2' => $xls_filter);
 
 &message ("Check number xls/BIFF8 roundtrip.");
 &test_roundtrip ($file,
 		 'format' => 'Gnumeric_Excel:excel_biff8',
-		 'ext' => "xls");
+		 'ext' => "xls",
+		 'filter2' => $xls_filter);
 
 &message ("Check number xlsx roundtrip.");
 &test_roundtrip ($file,
