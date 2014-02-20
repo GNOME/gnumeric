f5f35cf9fdf97ba92cd5b736746dad0cff47112e
Morten Welinder <terra@gnome.org>
2014-02-20 10:23:04 -0500

    Style test: silence biff7 complaints about 45 and -45 degree rotation.
    The format cannot handle those.

--- test/t6502-styles.pl
+++ test/t6502-styles.pl
@@ -18,21 +18,23 @@ my $file = "$samples/style-tests.gnumeric";
 		 'ext' => "ods",
 		 'filter2' => "$PERL -p -e '\$_ = \"\" if m{<meta:generator>}'");
 
-my $xls_filter = "$PERL -p -e '\$_ = \"\" if m{<meta:user-defined meta:name=.msole:codepage.}'";
+my $xls_codepage_filter = "$PERL -p -e '\$_ = \"\" if m{<meta:user-defined meta:name=.msole:codepage.}'";
+my $xls_rotation_filter = "$PERL -p -e 's{\\b(Rotation)=\"315\"}{\$1=\"270\"}; s{\\b(Rotation)=\"45\"}{\$1=\"0\"};'";
 
 &message ("Check style xls/BIFF7 roundtrip.");
 &test_roundtrip ($file,
 		 'format' => 'Gnumeric_Excel:excel_biff7',
 		 'ext' => "xls",
 		 'resize' => '16384x256',
-		 'filter2' => $xls_filter,
+		 'filter1' => $xls_rotation_filter,
+		 'filter2' => $xls_codepage_filter ,
 		 'ignore_failure' => 1);
 
 &message ("Check style xls/BIFF8 roundtrip.");
 &test_roundtrip ($file,
 		 'format' => 'Gnumeric_Excel:excel_biff8',
 		 'ext' => "xls",
-		 'filter2' => $xls_filter,
+		 'filter2' => $xls_codepage_filter,
 		 'ignore_failure' => 1);
 
 &message ("Check style xlsx roundtrip.");
