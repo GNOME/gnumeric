77f00e468c4104da1e3571381b09fc848b62d00e
Morten Welinder <terra@gnome.org>
2014-02-20 14:26:02 -0500

    Tests: add test for page setup roundtrip.

---
+++ test/t6503-page-setup.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -w
+# -----------------------------------------------------------------------------
+
+use strict;
+use lib ($0 =~ m|^(.*/)| ? $1 : ".");
+use GnumericTest;
+
+my $file = "$samples/page-setup-tests.gnumeric";
+
+&message ("Check page-setup gnumeric roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_XmlIO:sax',
+		 'ext' => "gnm");
+
+&message ("Check page-setup ods roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_OpenCalc:odf',
+		 'ext' => "ods",
+		 'filter2' => "$PERL -p -e '\$_ = \"\" if m{<meta:generator>}'",
+		 'ignore_failure' => 1);
+
+my $xls_codepage_filter = "$PERL -p -e '\$_ = \"\" if m{<meta:user-defined meta:name=.msole:codepage.}'";
+
+&message ("Check page-setup xls/BIFF7 roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_Excel:excel_biff7',
+		 'ext' => "xls",
+		 'resize' => '16384x256',
+		 'filter2' => $xls_codepage_filter ,
+		 'ignore_failure' => 1);
+
+&message ("Check page-setup xls/BIFF8 roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_Excel:excel_biff8',
+		 'ext' => "xls",
+		 'filter2' => $xls_codepage_filter,
+		 'ignore_failure' => 1);
+
+&message ("Check page-setup xlsx roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_Excel:xlsx',
+		 'ext' => "xlsx",
+		 'resize' => '1048576x16384',
+		 'ignore_failure' => 1);
