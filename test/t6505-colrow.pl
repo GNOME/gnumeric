92d81e854767b752b8b6cc0c5be0c6fabb2ae6fd
Morten Welinder <terra@gnome.org>
2014-02-24 20:50:08 -0500

    Tests: add test for column and row properties.

---
+++ test/t6505-colrow.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl -w
+# -----------------------------------------------------------------------------
+
+use strict;
+use lib ($0 =~ m|^(.*/)| ? $1 : ".");
+use GnumericTest;
+
+my $file = "$samples/colrow-tests.gnumeric";
+
+&message ("Check col/row info gnumeric roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_XmlIO:sax',
+		 'ext' => "gnm");
+
+&message ("Check col/row info ods roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_OpenCalc:odf',
+		 'ext' => "ods",
+		 'filter2' => "$PERL -p -e '\$_ = \"\" if m{<meta:generator>}'");
+
+my $xls_codepage_filter = "$PERL -p -e '\$_ = \"\" if m{<meta:user-defined meta:name=.msole:codepage.}'";
+
+&message ("Check col/row info xls/BIFF7 roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_Excel:excel_biff7',
+		 'ext' => "xls",
+		 'resize' => '16384x256',
+		 'filter2' => $xls_codepage_filter,
+		 'ignore_failure' => 1);
+
+&message ("Check col/row info xls/BIFF8 roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_Excel:excel_biff8',
+		 'ext' => "xls",
+		 'filter2' => $xls_codepage_filter,
+		 'ignore_failure' => 1);
+
+&message ("Check col/row info xlsx roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_Excel:xlsx',
+		 'ext' => "xlsx",
+		 'resize' => '1048576x16384');
