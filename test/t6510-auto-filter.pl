fd0fe1209e81093a577a697ebefc665709f741af
Morten Welinder <terra@gnome.org>
2014-03-01 13:23:49 -0500

    Tests: new test for auto-filter.

---
+++ test/t6510-auto-filter.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -w
+# -----------------------------------------------------------------------------
+
+use strict;
+use lib ($0 =~ m|^(.*/)| ? $1 : ".");
+use GnumericTest;
+
+my $file = "$samples/auto-filter-tests.gnumeric";
+
+&message ("Check auto-filter gnumeric roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_XmlIO:sax',
+		 'ext' => "gnm");
+
+&message ("Check auto-filter ods roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_OpenCalc:odf',
+		 'ext' => "ods",
+		 'filter2' => "$PERL -p -e '\$_ = \"\" if m{<meta:generator>}'",
+		 'ignore_failure' => 1);
+
+my $xls_codepage_filter = "$PERL -p -e '\$_ = \"\" if m{<meta:user-defined meta:name=.msole:codepage.}'";
+
+my $xls_greek_filter = "$PERL -p -C7 -e '1 while (s{\\b((Title|Message)=\".*Greek[ ?]+)[^ ?\"]}{\$1?})'";
+
+&message ("Check auto-filter xls/BIFF7 roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_Excel:excel_biff7',
+		 'ext' => "xls",
+		 'resize' => '16384x256',
+		 'filter1' => $xls_greek_filter,
+		 'filter2' => $xls_codepage_filter);
+
+&message ("Check auto-filter xls/BIFF8 roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_Excel:excel_biff8',
+		 'ext' => "xls",
+		 'filter2' => $xls_codepage_filter);
+
+&message ("Check auto-filter xlsx roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_Excel:xlsx',
+		 'ext' => "xlsx",
+		 'resize' => '1048576x16384');
