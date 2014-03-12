48ebe17c2ce9425c1e9ce599b4c0fc6df9029af1
Morten Welinder <terra@gnome.org>
2014-03-12 13:09:59 -0400

    Tests: add initial tests for rich text.
    Excels formats all fail.

---
+++ test/t6514-rich-text.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl -w
+# -----------------------------------------------------------------------------
+
+# NOTE: this is an import/export tests.  We do not look at what is done
+# with the formats in the test files.
+
+use strict;
+use lib ($0 =~ m|^(.*/)| ? $1 : ".");
+use GnumericTest;
+
+my $file = "$samples/rich-text-tests.gnumeric";
+
+&message ("Check rich text gnumeric roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_XmlIO:sax',
+		 'ext' => "gnm");
+
+&message ("Check rich text ods roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_OpenCalc:odf',
+		 'ext' => "ods",
+		 'filter2' => "$PERL -p -e '\$_ = \"\" if m{<meta:generator>}'");
+
+my $xls_codepage_filter = "$PERL -p -e '\$_ = \"\" if m{<meta:user-defined meta:name=.msole:codepage.}'";
+
+&message ("Check rich text xls/BIFF7 roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_Excel:excel_biff7',
+		 'ext' => "xls",
+		 'resize' => '16384x256',
+		 'filter2' => $xls_codepage_filter,
+		 'ignore_failure' => 1);
+
+&message ("Check rich text xls/BIFF8 roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_Excel:excel_biff8',
+		 'ext' => "xls",
+		 'filter2' => $xls_codepage_filter,
+		 'ignore_failure' => 1);
+
+&message ("Check rich text xlsx roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_Excel:xlsx',
+		 'ext' => "xlsx",
+		 'resize' => '1048576x16384',
+		 'ignore_failure' => 1);
