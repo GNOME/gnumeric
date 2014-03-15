6cd9532702e09680df983cbf2595abd44a0a8a48
Morten Welinder <terra@gnome.org>
2014-03-15 13:37:07 -0400

    t6515: don't test xlsx until we start implementing.
    It's just noise at this point.

--- test/t6515-cond-format.pl
+++ test/t6515-cond-format.pl
@@ -41,9 +41,11 @@ my $xls_cond_format_filter = "$PERL -p -e 's{\\s+Format=\"[^\"\"]*\"}{} if m{<gn
 		 'filter2' => $xls_codepage_filter,
 		 'ignore_failure' => 1);
 
+if (0) {
 &message ("Check conditional format xlsx roundtrip.");
 &test_roundtrip ($file,
 		 'format' => 'Gnumeric_Excel:xlsx',
 		 'ext' => "xlsx",
 		 'resize' => '1048576x16384',
 		 'ignore_failure' => 1);
+}
