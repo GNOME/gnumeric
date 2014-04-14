a58128bf55b3c4a4b30c398875303ce4d9defb73
Morten Welinder <terra@gnome.org>
2014-04-14 16:04:46 -0400

    t6516: don't test biff7.
    We don't save graphs in biff7, so don't test.

--- test/t6516-graph.pl
+++ test/t6516-graph.pl
@@ -22,23 +22,20 @@ my $file = "$samples/graph-tests.gnumeric";
 
 my $xls_codepage_filter = "$PERL -p -e '\$_ = \"\" if m{<meta:user-defined meta:name=.msole:codepage.}'";
 
-# xls/biff cannot handle format in graphs.
-my $xls_cond_format_filter = "$PERL -p -e 'if (m{<gnm:Condition\\b} ... m{</gnm:Condition\\b}) { s{\\s+Format=\"[^\"\"]*\"}{}; }'";
-
-&message ("Check graph xls/BIFF7 roundtrip.");
-&test_roundtrip ($file,
-		 'format' => 'Gnumeric_Excel:excel_biff7',
-		 'ext' => "xls",
-		 'resize' => '16384x256',
-		 # 'filter1' => $xls_cond_format_filter,
-		 'filter2' => $xls_codepage_filter,
-		 'ignore_failure' => 1);
+if (0) {
+    # We don't save graphs, so don't test.
+    &message ("Check graph xls/BIFF7 roundtrip.");
+    &test_roundtrip ($file,
+		     'format' => 'Gnumeric_Excel:excel_biff7',
+		     'ext' => "xls",
+		     'resize' => '16384x256',
+		     'filter2' => $xls_codepage_filter);
+}
 
 &message ("Check graph xls/BIFF8 roundtrip.");
 &test_roundtrip ($file,
 		 'format' => 'Gnumeric_Excel:excel_biff8',
 		 'ext' => "xls",
-		 '# filter1' => $xls_cond_format_filter,
 		 'filter2' => $xls_codepage_filter,
 		 'ignore_failure' => 1);
 
