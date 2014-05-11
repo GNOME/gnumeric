eb8b7f3b3ba99b9e5a3a7baa1a6b4e0070232c13
Morten Welinder <terra@gnome.org>
2014-05-11 11:24:48 -0400

    ods: roundtrip improvement for lengths.

--- plugins/openoffice/openoffice-write.c
+++ plugins/openoffice/openoffice-write.c
@@ -564,8 +564,8 @@ static void
 odf_add_pt (GsfXMLOut *xml, char const *id, double l)
 {
 	GString *str = g_string_new (NULL);
-
-	g_string_append_printf (str, "%.6fpt", l);
+	go_dtoa (str, "!g", l);
+	g_string_append (str, "pt");
 	gsf_xml_out_add_cstr_unchecked (xml, id, str->str);
 	g_string_free (str, TRUE);
 }
