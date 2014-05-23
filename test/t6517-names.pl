731d10cd447dd5bcb01901d5ab3e3b3286f9b0d1
Morten Welinder <terra@gnome.org>
2014-05-22 21:14:25 -0400

    tests: start test file for defined names

---
+++ test/t6517-names.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl -w
+# -----------------------------------------------------------------------------
+
+use strict;
+use lib ($0 =~ m|^(.*/)| ? $1 : ".");
+use GnumericTest;
+
+my $file = "$samples/names-tests.gnumeric";
+
+&message ("Check names gnumeric roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_XmlIO:sax',
+		 'ext' => "gnm");
+
+&message ("Check names ods roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_OpenCalc:odf',
+		 'ext' => "ods",
+		 'filter2' => "$PERL -p -e '\$_ = \"\" if m{<meta:generator>}'");
+
+my $xls_codepage_filter = "$PERL -p -e '\$_ = \"\" if m{<meta:user-defined meta:name=.msole:codepage.}'";
+
+&message ("Check names xls/BIFF7 roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_Excel:excel_biff7',
+		 'ext' => "xls",
+		 'resize' => '16384x256',
+		 'filter2' => $xls_codepage_filter,
+		 'ignore_failure' => 1);
+
+&message ("Check names xls/BIFF8 roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_Excel:excel_biff8',
+		 'ext' => "xls",
+		 'filter2' => $xls_codepage_filter,
+		 'ignore_failure' => 1);
+
+&message ("Check names xlsx roundtrip.");
+&test_roundtrip ($file,
+		 'format' => 'Gnumeric_Excel:xlsx',
+		 'ext' => "xlsx",
+		 'resize' => '1048576x16384',
+		 'ignore_failure' => 1);
