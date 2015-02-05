dbb38c8a6fe797cf5ad317b0ba51989bc287596a
Morten Welinder <terra@gnome.org>
2015-02-05 11:14:31 -0500

    tests: add tests for sheet objects.
    So far passive objects only.  That will change.

---
+++ test/t6518-objects.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl -w
+# -----------------------------------------------------------------------------
+
+use strict;
+use lib ($0 =~ m|^(.*/)| ? $1 : ".");
+use GnumericTest;
+
+my $file = "$samples/object-tests.gnumeric";
+$GnumericTest::default_subtests = '*,-biff7';
+
+
+if (&subtest ("gnumeric")) {
+    &message ("Check object gnumeric roundtrip.");
+    &test_roundtrip ($file,
+		     'format' => 'Gnumeric_XmlIO:sax',
+		     'ext' => "gnm");
+}
+
+if (&subtest ("ods")) {
+    &message ("Check object ods roundtrip.");
+    &test_roundtrip ($file,
+		     'format' => 'Gnumeric_OpenCalc:odf',
+		     'ext' => "ods",
+		     'filter2' => 'std:drop_generator',
+		     'ignore_failure' => 1);
+}
+
+if (&subtest ("biff7")) {
+    # We don't save objects, so don't test.
+    &message ("Check object xls/BIFF7 roundtrip.");
+    &test_roundtrip ($file,
+		     'format' => 'Gnumeric_Excel:excel_biff7',
+		     'ext' => "xls",
+		     'resize' => '16384x256',
+		     'filter2' => 'std:drop_codepage');
+}
+
+if (&subtest ("biff8")) {
+    &message ("Check object xls/BIFF8 roundtrip.");
+    &test_roundtrip ($file,
+		     'format' => 'Gnumeric_Excel:excel_biff8',
+		     'ext' => "xls",
+		     'filter2' => 'std:drop_codepage',
+		     'ignore_failure' => 1);
+}
+
+if (&subtest ("xlsx")) {
+    &message ("Check object xlsx roundtrip.");
+    &test_roundtrip ($file,
+		     'format' => 'Gnumeric_Excel:xlsx',
+		     'ext' => "xlsx",
+		     'resize' => '1048576x16384',
+		     'ignore_failure' => 1);
+}
