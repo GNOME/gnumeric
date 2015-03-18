c8fb159a79e09986627ce07b5975a4896e0e6657
Andreas J. Guelzow <aguelzow@pyrshep.ca>
2015-03-17 19:39:26 -0600

    Improve ODF export of additional axislines. [#746321]
    2015-02-25  Andreas J. Guelzow <aguelzow@pyrshep.ca>
    
    	* openoffice-write.c (odf_write_axis_full): the ODF schema allows multiple
    	chart:axis with the same dimension

--- plugins/openoffice/openoffice-write.c
+++ plugins/openoffice/openoffice-write.c
@@ -7853,20 +7853,18 @@ odf_write_axis_full (GnmOOExport *state,
 		GogObject const *axis = l->data;
 		if (axis != NULL) {
 			int id = gog_object_get_id (GOG_OBJECT (axis));
-			if (state->with_extension || id == 1) {
-				gsf_xml_out_start_element (state->xml, 
-							   (id == 1) ? CHART "axis" : GNMSTYLE "axis");
-				gsf_xml_out_add_cstr (state->xml, CHART "dimension", dimension);
-				if (state->with_extension)
-					gsf_xml_out_add_int (state->xml, GNMSTYLE "id", id);
-				gsf_xml_out_add_cstr (state->xml, CHART "style-name", style_label);
-				odf_write_label (state, axis);
-				if (include_cats)
-					odf_write_axis_categories (state, series);
-				odf_write_axis_grid (state, axis);
-				odf_write_axislines (state, axis);
-				gsf_xml_out_end_element (state->xml); /* </chart:axis> or </gnm:axis>*/
-			}
+
+			gsf_xml_out_start_element (state->xml, CHART "axis");
+			gsf_xml_out_add_cstr (state->xml, CHART "dimension", dimension);
+			if (state->with_extension)
+				gsf_xml_out_add_int (state->xml, GNMSTYLE "id", id);
+			gsf_xml_out_add_cstr (state->xml, CHART "style-name", style_label);
+			odf_write_label (state, axis);
+			if (include_cats)
+				odf_write_axis_categories (state, series);
+			odf_write_axis_grid (state, axis);
+			odf_write_axislines (state, axis);
+			gsf_xml_out_end_element (state->xml); /* </chart:axis> */
 		}
 	}
 	g_slist_free (children);
