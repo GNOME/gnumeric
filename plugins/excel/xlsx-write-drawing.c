7a9ccf1d3ef894d4555050018369174426d91899
Morten Welinder <terra@gnome.org>
2015-03-23 19:38:11 -0400

    xlsx: fix schema problem

--- plugins/excel/xlsx-write-drawing.c
+++ plugins/excel/xlsx-write-drawing.c
@@ -628,6 +628,8 @@ xlsx_write_chart_text (XLSXWriteState *state, GsfXMLOut *xml,
 	gsf_xml_out_end_element (xml); /* </c:rich> */
 	gsf_xml_out_end_element (xml); /* </c:tx> */
 
+	xlsx_write_chart_uint (xml, "c:overlay", 0);
+
 	style_minus_font = go_style_dup (style);
 	style_minus_font->interesting_fields &= ~GO_STYLE_FONT;
 	xlsx_write_go_style (xml, state, style_minus_font);
@@ -728,7 +730,6 @@ xlsx_write_axis (XLSXWriteState *state, GsfXMLOut *xml, GogPlot *plot, GogAxis *
 		if (text != NULL) {
 			gsf_xml_out_start_element (xml, "c:title");
 			xlsx_write_chart_text (state, xml, text, label);
-			xlsx_write_chart_uint (xml, "c:overlay", 0);
 			gsf_xml_out_end_element (xml);
 		}
 	}
@@ -1157,7 +1158,6 @@ xlsx_write_one_chart (XLSXWriteState *state, GsfXMLOut *xml, GogObject const *ch
 		if (text != NULL) {
 			gsf_xml_out_start_element (xml, "c:title");
 			xlsx_write_chart_text (state, xml, text, obj);
-			xlsx_write_chart_uint (xml, "c:overlay", 0);
 			gsf_xml_out_end_element (xml);
 		}
 	}
