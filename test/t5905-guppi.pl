3ffc6bb5d437f9fa5757e70270cfb19820cb1835
Morten Welinder <terra@gnome.org>
2015-04-01 14:15:22 -0400

    test: add test for guppi graph format.

---
+++ test/t5905-guppi.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl -w
+# -----------------------------------------------------------------------------
+
+use strict;
+use lib ($0 =~ m|^(.*/)| ? $1 : ".");
+use GnumericTest;
+
+# See test_importer comments for mode definitions.
+my $mode = ((shift @ARGV) || "check");
+
+&message ("Check the mps importer.");
+&test_importer ("$samples/b66666-guppi.gnumeric", "4efa15c01d6c907ef13b2098d650129647447e97", $mode);
