ec5b5874bbd690b5133ab92e35e88c18adb0a46c
Morten Welinder <terra@gnome.org>
2016-03-05 16:05:52 -0500

    Tests: add gsl's tests for complex number functions.

---
+++ test/t1014-gsl.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -w
+# -----------------------------------------------------------------------------
+
+use strict;
+use lib ($0 =~ m|^(.*/)| ? $1 : ".");
+use GnumericTest;
+
+my $file = "gsl.gnumeric";
+&message ("Check that $file evaluates correctly.");
+&test_sheet_calc ("$samples/$file", "C2:F2",
+		  sub {
+		      foreach my $a (split (',')) {
+			  return 0 unless $a > 14.4;
+		      }
+		      return 1;
+		  });
