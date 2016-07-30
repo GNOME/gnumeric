f1152dcefeef380dba593f9aff9fcec82dd0b198
Jean Brefort <jean.brefort@normalesup.org>
2016-07-30 07:51:13 +0200

    fix build with pxlib-0.6.7. [#769319]

--- plugins/paradox/px.h
+++ plugins/paradox/px.h
@@ -3,6 +3,10 @@
 
 #include <gnumeric.h>
 #include <pxversion.h>
+/* if we are there, pxlib has been built with gsf support, so we need
+ * to define HAVE_GSF at least for pxlib-0.6.7 see #769319 and
+ * https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=832827 */
+#define HAVE_GSF 1
 #include <paradox-gsf.h>
 
 #define PX_MEMORY_DEBUGGING
