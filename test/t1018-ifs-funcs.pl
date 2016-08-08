5e8ea469d71882dc09ba71c041f27da2eeb84195
Morten Welinder <terra@gnome.org>
2016-08-08 14:18:33 -0400

    Tests: add new ifs-funcs to tests

---
+++ test/t1018-ifs-funcs.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl -w
+# -----------------------------------------------------------------------------
+
+use strict;
+use lib ($0 =~ m|^(.*/)| ? $1 : ".");
+use GnumericTest;
+
+my $file = "excel12/ifs-funcs.xlsx";
+&message ("Check that $file evaluates correctly.");
+&test_sheet_calc ("$samples/$file", "Overview!C2:C99", sub { /(\s*0)+\s*/i });
