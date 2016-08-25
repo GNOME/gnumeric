1683c0571126132794d9c05653e9a3f815ae24d2
Morten Welinder <terra@gnome.org>
2016-08-24 21:00:05 -0400

    Docs: first cut at using yelp tools.

--- src/wbc-gtk-actions.c
+++ src/wbc-gtk-actions.c
@@ -1062,7 +1062,7 @@ static GNM_ACTION_DEF (cb_help_docs)
 
 #ifndef G_OS_WIN32
 	argv[0] = (char *)"yelp";
-	argv[1] = (char *)"ghelp:gnumeric";
+	argv[1] = (char *)"help:gnumeric";
 	g_spawn_async (NULL, argv, NULL,
 		       G_SPAWN_SEARCH_PATH | G_SPAWN_STDOUT_TO_DEV_NULL
 		       | G_SPAWN_STDERR_TO_DEV_NULL,
