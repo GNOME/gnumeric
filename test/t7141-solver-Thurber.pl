95d0684a55d995a1b09b37feeb8209236c8bd636
Morten Welinder <terra@gnome.org>
2016-09-11 18:43:45 -0400

    Tests: the solver tests now succeed.

--- test/t7141-solver-Thurber.pl
+++ test/t7141-solver-Thurber.pl
@@ -6,18 +6,12 @@ use lib ($0 =~ m|^(.*/)| ? $1 : ".");
 use GnumericTest;
 
 my $file = "Thurber.gnumeric";
-my $rle = 3.8;
+my $rle = 5;
 &message ("Check non-linear solver on $file problem.");
 &test_sheet_calc ("$samples/solver/$file", ['--solve'], "K39",
 		  sub {
 		      chomp;
 		      print STDERR "--> $_\n";
 		      my $ok = (/^[-+]?(\d|\.\d)/ && $_ > $rle);
-		      if ($ok) {
-			  print STDERR "Unexpected success.\n";
-		      } else {
-			  print STDERR "Known failure.\n" ;
-			  $ok = 1;
-		      }
 		      return $ok;
 		  });
