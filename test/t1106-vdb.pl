fd1dcae100c1743fcb4322a42dbf84ec7546b6dc
Morten Welinder <terra@gnome.org>
2017-11-13 19:52:58 -0500

    Tests: new test for VDB.

---
+++ test/t1106-vdb.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl -w
+# -----------------------------------------------------------------------------
+
+use strict;
+use lib ($0 =~ m|^(.*/)| ? $1 : ".");
+use GnumericTest;
+
+my $file = "vdb.gnumeric";
+&message ("Check that $file evaluates correctly.");
+&test_sheet_calc ("$samples/$file", "A1", sub { /\bOK\b/i });
