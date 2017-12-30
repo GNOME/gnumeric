53efa64bcd9f9f3c1cd5e0930b951bcb89613bae
Morten Welinder <terra@gnome.org>
2017-12-29 22:41:06 -0500

    tests: add test for CRONBACH

---
+++ test/t1107-cronbach.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl -w
+# -----------------------------------------------------------------------------
+
+use strict;
+use lib ($0 =~ m|^(.*/)| ? $1 : ".");
+use GnumericTest;
+
+my $file = "cronbach.gnumeric";
+&message ("Check that $file evaluates correctly.");
+&test_sheet_calc ("$samples/$file", "B2", sub { /\bTRUE\b/i });
