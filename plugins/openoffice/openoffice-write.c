c40c638ec5358a9450552dbdfa2f2264d15f4f7c
Andreas J. Guelzow <aguelzow@pyrshep.ca>
2020-05-09 00:43:28 -0600

    Fix image name roundtrip through ODF.
    2020-05-09  Andreas J. Guelzow <aguelzow@pyrshep.ca>
    
    	* openoffice-write.c (odf_write_image): do not write two names

--- plugins/openoffice/openoffice-write.c
+++ plugins/openoffice/openoffice-write.c
@@ -3061,14 +3061,6 @@ odf_write_image (GnmOOExport *state, SheetObject *so, char const *name)
 			      "image", &image,
 			      NULL);
 
-		if (image) {
-			/* Write attribute for surrounding draw:frame */
-			const char *image_name = go_image_get_name (image);
-			if (image_name)
-				gsf_xml_out_add_cstr (state->xml, DRAW "name", image_name);
-			g_object_unref (image);
-		}
-
 		fullname = g_strdup_printf ("Pictures/%s.%s", name, image_type);
 
 		gsf_xml_out_start_element (state->xml, DRAW "image");
