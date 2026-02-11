0de4c0a45f078ec211fd372da4103b09cb718b1b
botantony <antonsm21@gmail.com>
2026-02-11 15:07:15 +0100

    sstest: replace `bool` type with `gboolean`
    Related to https://github.com/Homebrew/homebrew-core/pull/266893
    
    Signed-off-by: botantony <antonsm21@gmail.com>

--- src/sstest.c
+++ src/sstest.c
@@ -898,7 +898,7 @@ test_insert_delete (void)
 /* ------------------------------------------------------------------------- */
 
 static gboolean
-check_help_expression (const char *text, GnmFunc const *fd, bool localized)
+check_help_expression (const char *text, GnmFunc const *fd, gboolean localized)
 {
 	GnmConventions const *convs = gnm_conventions_default;
 	GnmParsePos pp;
