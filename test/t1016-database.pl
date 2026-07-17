02e5de0a4674b661b0685aed08eb8f9be4db0cf3
Morten Welinder <terra@gnome.org>
2026-07-17 16:51:52 -0400

    Fix checking
    This only checked that one page worked right.

--- test/t1016-database.pl
+++ test/t1016-database.pl
@@ -7,4 +7,4 @@ use GnumericTest;
 
 my $file = "excel12/database.xlsx";
 &message ("Check that $file evaluates correctly.");
-&test_sheet_calc ("$samples/$file", "Overview!C2:C99", sub { /(\s*0)+\s*/i });
+&test_sheet_calc ("$samples/$file", "Overview!C2:C99", sub { /^(\s*0\b\s*)*$/i });
